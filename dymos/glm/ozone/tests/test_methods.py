22c0c641486083994295bf0d4bc98ffdba588b18
John T. Hwang <hwangjt@umich.edu>
2018-04-04 14:03:17 -0400

    minor fix

--- dymos/glm/ozone/tests/test_methods.py
+++ dymos/glm/ozone/tests/test_methods.py
@@ -72,7 +72,8 @@ class OzoneODETestCase(unittest.TestCase):
         p.model.add_subsystem('dummy_comp', IndepVarComp('dummy_var'))
         p.model.add_objective('dummy_comp.dummy_var')
 
-        phase.set_time_options(fix_initial=True, initial_bounds=(0, 0), duration_bounds=(tf, tf))
+        phase.set_time_options(initial_bounds=(0, 0), duration_bounds=(tf, tf))
+        phase.set_state_options('y', fix_initial=True)
 
         if glm_formulation == 'optimizer-based':
             p.driver = ScipyOptimizeDriver()
