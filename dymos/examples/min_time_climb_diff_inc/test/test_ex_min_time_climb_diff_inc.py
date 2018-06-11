dbb57c7f99c89546190b62d5a4aa4f40debfc507
Rob Falck <rfalck@nasa.gov>
2018-06-11 11:24:16 -0400

    another go at skipping when SNOPT is absent

--- dymos/examples/min_time_climb_diff_inc/test/test_ex_min_time_climb_diff_inc.py
+++ dymos/examples/min_time_climb_diff_inc/test/test_ex_min_time_climb_diff_inc.py
@@ -9,15 +9,8 @@ from openmdao.utils.assert_utils import assert_rel_error
 import dymos.examples.min_time_climb_diff_inc.ex_min_time_climb_diff_inc as \
     ex_min_time_climb_diff_inc
 
-try:
-    from pyoptsparse import SNOPT
-except ImportError:
-    SNOPT = None
-
-
 class TestExampleMinTimeClimbDiffInc(unittest.TestCase):
 
-    @unittest.skipIf(SNOPT is None, 'This example requires pyoptsparse SNOPT')
     @parameterized.expand(
         itertools.product(['gauss-lobatto', 'radau-ps'],  # transcription
                           ['csc'],  # jacobian
@@ -27,11 +20,15 @@ class TestExampleMinTimeClimbDiffInc(unittest.TestCase):
     )
     def test_results(self, transcription='gauss-lobatto', jacobian='csc'):
         ex_min_time_climb_diff_inc.SHOW_PLOTS = False
-        p = ex_min_time_climb_diff_inc.min_time_climb_diff_inc(optimizer='SNOPT',
-                                                               num_seg=5,
-                                                               transcription_order=7,
-                                                               transcription=transcription,
-                                                               top_level_jacobian=jacobian)
+        try:
+            p = ex_min_time_climb_diff_inc.min_time_climb_diff_inc(optimizer='SNOPT',
+                                                                   num_seg=5,
+                                                                   transcription_order=7,
+                                                                   transcription=transcription,
+                                                                   top_level_jacobian=jacobian)
+        except ImportError as e:
+            if str(e) == 'Optimizer SNOPT is not available in this installation.':
+                self.skipTest(str(e))
         # Check that time matches to within 1% of an externally verified solution.
         assert_rel_error(self, p.model.phase0.get_values('time')[-1], 321.0, tolerance=0.01)
 
