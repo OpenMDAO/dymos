ced96f14564c84b18208a714c7b454dfffee9025
kmarsteller <keith.g.marsteller@nasa.gov>
2018-08-09 16:29:43 -0400

    fixing merge conflicts2

--- dymos/docs/conf.py
+++ dymos/docs/conf.py
@@ -24,15 +24,15 @@ from numpydoc.docscrape import NumpyDocString, Reader
 
 openmdao_path = os.path.split(os.path.abspath(openmdao.__file__))[0]
 
-# from openmdao.docs._utils.patch import do_monkeypatch
-# from openmdao.docs._utils.generate_sourcedocs import generate_docs
-# sys.path.insert(0, os.path.join(openmdao_path, 'docs', '_exts'))
-# sys.path.insert(0, os.path.join(openmdao_path, 'docs', '_utils'))
+from openmdao.docs._utils.patch import do_monkeypatch
+from openmdao.docs._utils.generate_sourcedocs import generate_docs
+sys.path.insert(0, os.path.join(openmdao_path, 'docs', '_exts'))
+sys.path.insert(0, os.path.join(openmdao_path, 'docs', '_utils'))
 
-from openmdao.docs.utils.generate_sourcedocs import generate_docs
-from openmdao.docs.utils.patch import do_monkeypatch
-sys.path.insert(0, os.path.join(openmdao_path, 'docs', 'exts'))
-sys.path.insert(0, os.path.join(openmdao_path, 'docs', 'utils'))
+# from openmdao.docs.utils.generate_sourcedocs import generate_docs
+# from openmdao.docs.utils.patch import do_monkeypatch
+# sys.path.insert(0, os.path.join(openmdao_path, 'docs', 'exts'))
+# sys.path.insert(0, os.path.join(openmdao_path, 'docs', 'utils'))
 
 do_monkeypatch()
 
