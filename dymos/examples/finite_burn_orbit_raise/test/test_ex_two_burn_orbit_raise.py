91e7b08e907fcfff771d77d006cafa82e691907c
Bret Naylor <naylor.b@gmail.com>
2018-09-14 11:18:20 -0400

    re-cloned and updated changed files

--- dymos/examples/finite_burn_orbit_raise/test/test_ex_two_burn_orbit_raise.py
+++ dymos/examples/finite_burn_orbit_raise/test/test_ex_two_burn_orbit_raise.py
@@ -2,6 +2,9 @@ from __future__ import print_function, division, absolute_import
 
 import os
 import unittest
+from shutil import rmtree
+from tempfile import mkdtemp
+import errno
 
 from openmdao.utils.assert_utils import assert_rel_error
 from openmdao.utils.general_utils import set_pyoptsparse_opt
@@ -12,20 +15,33 @@ from dymos.examples.finite_burn_orbit_raise.ex_two_burn_orbit_raise import \
 
 class TestExampleTwoBurnOrbitRaise(unittest.TestCase):
 
-    @classmethod
-    def tearDownClass(cls):
-        for filename in ['coloring.json', 'two_burn_orbit_raise_example.db', 'SLSQP.out',
-                         'traj_sim.db']:
-            if os.path.exists(filename):
-                os.remove(filename)
+    def setUp(self):
+        self.orig_dir = os.getcwd()
+        self.temp_dir = mkdtemp()
+        os.chdir(self.temp_dir)
+
+    def tearDown(self):
+        os.chdir(self.orig_dir)
+        try:
+            rmtree(self.temp_dir)
+        except OSError as e:
+            # If directory already deleted, keep going
+            if e.errno not in (errno.ENOENT, errno.EACCES, errno.EPERM):
+                raise e
 
     def test_ex_two_burn_orbit_raise(self):
         _, optimizer = set_pyoptsparse_opt('SNOPT', fallback=False)
 
         p = two_burn_orbit_raise_problem(transcription='gauss-lobatto', transcription_order=3,
-                                         compressed=True, show_plots=False, optimizer=optimizer)
+                                         compressed=True, show_plots=False, optimizer=optimizer,
+                                         show_output=False)
 
-        assert_rel_error(self, p.get_val('burn2.states:deltav')[-1], 0.3995, tolerance=2.0E-3)
+        if p.model.traj.phases.burn2 in p.model.traj.phases._subsystems_myproc:
+            assert_rel_error(self, p.get_val('traj.burn2.states:deltav')[-1], 0.3995, tolerance=2.0E-3)
+
+
+class TestExampleTwoBurnOrbitRaiseMPI(TestExampleTwoBurnOrbitRaise):
+    N_PROCS = 3
 
 
 if __name__ == '__main__':
