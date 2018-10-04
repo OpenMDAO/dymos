cad7730528a5f6a03606ea8cb4f90c5bf61fa6b6
Justin Gray <justin.s.gray@gmail.com>
2018-10-04 11:21:38 -0400

    nlrk works

---
+++ dymos/phases/explicit/components/segment/nl_rk_solver.py
@@ -0,0 +1,85 @@
+"""
+Define the NonlinearRK class.
+
+Custom nonlinear solver that time-steps through the integration of an ODE using rk4
+"""
+
+import numpy as np
+
+from openmdao.core.system import System
+from openmdao.recorders.recording_iteration_stack import Recording
+from openmdao.solvers.solver import NonlinearSolver
+from openmdao.utils.mpi import multi_proc_fail_check
+
+from dymos.utils.simulation import ScipyODEIntegrator
+
+
+def _single_rk4_step(f, h, t0, y0):
+    """takes a single RK4 step in time"""
+
+    K_ = np.zeros(4)
+
+
+    K0 = K_[0] = h*f([t0],[y0])
+    K1 = K_[1] = h*f([t0+h/2.],[y0+K0/2.])
+    K2 = K_[2] = h*f([t0+h/2.],[y0+K1/2.])
+    K_[3] = h*f([t0+h],[y0+K2])
+
+    next_y = y0 + np.sum(K_*np.array([1, 2, 2, 1]))/6.
+    return next_y, K_
+
+class NonlinearRK(NonlinearSolver):
+
+    SOLVER = 'NL: RK'
+
+    def solve(self):
+        """
+        Run the solver.
+
+        Returns
+        -------
+        boolean
+            Failure flag; True if failed to converge, False is successful.
+        float
+            absolute error.
+        float
+            relative error.
+        """
+        system = self._system
+
+        f = self.options['ODE_integrator']._f_ode
+
+        N_STEPS = system.options['num_steps']
+        seg_t0, seg_tf = system._inputs['seg_t0_tf']
+        h = ((seg_tf - seg_t0) / N_STEPS) * np.ones(N_STEPS)
+        t = np.linspace(seg_t0, seg_tf, N_STEPS + 1)
+        y0 = system._inputs['initial_states:y']
+
+
+        y = system._outputs['step_states:y']
+        y[0]= y0
+
+        K =system._outputs['k:y']
+
+        for i in range(N_STEPS):
+            # print(i, h[i], t[i], y[i])
+            yn, Kn = _single_rk4_step(f, h[i], t[i], y[i])
+            y[i+1] = yn
+            system._outputs['k:y'][i] = Kn.reshape((4,1))
+
+        # TODO: optionally check the residual values to ensure the RK was stable
+        #       only do this optionally, because it will require an additional
+        #       call to ODE which might be expensive
+
+
+
+    def _declare_options(self):
+        """
+        Declare options before kwargs are processed in the init method.
+        """
+
+        # NOTE: if possible we probably don't want to leave this integrator allocated when solve is not being done.
+        #       it might make sense to have the solver create this itself, using information from the parent class.
+        #       For now this works fine though.
+        self.options.declare('ODE_integrator', types=ScipyODEIntegrator, desc='callable function to compute y_dot = f(u,y,args)')
+
--- dymos/phases/explicit/components/segment/tests/test_segment_group.py
+++ dymos/phases/explicit/components/segment/tests/test_segment_group.py
@@ -12,7 +12,8 @@ from openmdao.utils.assert_utils import assert_rel_error, assert_check_partials
 from dymos import declare_state, declare_time
 from dymos.phases.options import TimeOptionsDictionary
 from dymos.phases.explicit.components.segment.segment_group import ExplicitSegment
-
+from dymos.phases.explicit.components.segment.nl_rk_solver import NonlinearRK
+from dymos.utils.simulation import ScipyODEIntegrator
 
 @declare_time(targets=['t'], units='s')
 @declare_state('y', targets=['y'], rate_source='ydot', units='m')
@@ -61,7 +62,7 @@ class TestExplicitSegmentSimpleIntegration(unittest.TestCase):
         cls.p.model.add_subsystem('segment', seg)
 
         cls.p.model.connect('seg_t0_tf', 'segment.seg_t0_tf')
-        cls.p.model.connect('y_0', ('segment.states:y_0'))
+        cls.p.model.connect('y_0', ('segment.initial_states:y'))
 
         for state_name, options in iteritems(state_opts):
             cls.p.model.connect('segment.stage_ode.{0}'.format(options['rate_source']),
@@ -79,11 +80,45 @@ class TestExplicitSegmentSimpleIntegration(unittest.TestCase):
                             src_indices=np.arange(16, dtype=int),
                             flat_src_indices=True)
 
-        cls.p.setup(check=True, force_alloc_complex=True)
+    def test_results_nlbgs(self):
+        """make sure you get the right answer using the NLBGS solver"""
+        self.p.setup(check=False, force_alloc_complex=True)
+        self.p.model.segment.nonlinear_solver.options['iprint'] = -1
 
-        cls.p.run_model()
+        self.p.run_model()
 
-    def test_results(self):
+        y_expected = np.array([[0.5],
+                               [1.425130208333333],
+                               [2.639602661132812],
+                               [4.006818970044454],
+                               [5.301605229265987]])
+
+        assert_rel_error(self,
+                         self.p['segment.step_states:y'],
+                         y_expected,
+                         tolerance=1.0E-9)
+
+    def test_results_nlrk(self):
+        """make sure you get the right answer using the custom nl_rk solver"""
+
+        self.p.setup(check=False, force_alloc_complex=True)
+
+        seg = self.p.model.segment
+
+        integrator = ScipyODEIntegrator(phase_name='',
+                                        ode_class=seg.options['ode_class'],
+                                        ode_init_kwargs=seg.options['ode_init_kwargs'],
+                                        state_options=seg.options['state_options'],
+                                        time_options=seg.options['time_options'],
+                                        control_options={},
+                                        design_parameter_options={},
+                                        input_parameter_options={})
+
+        integrator.setup(check=False)
+
+        seg.nonlinear_solver = NonlinearRK(ODE_integrator=integrator)
+
+        self.p.run_model()
 
         y_expected = np.array([[0.5],
                                [1.425130208333333],
@@ -98,8 +133,15 @@ class TestExplicitSegmentSimpleIntegration(unittest.TestCase):
 
     def test_partials(self):
 
-        cpd = self.p.check_partials(method='cs', compact_print=True)
+        self.p.setup(check=False, force_alloc_complex=True)
+        self.p.model.segment.nonlinear_solver.options['iprint'] = -1
+
+        self.p.run_model()
+
+        cpd = self.p.check_partials(method='cs', compact_print=True, out_stream=None)
         assert_check_partials(cpd)
 
 
+if __name__ == "__main__":
+    unittest.main()
 
