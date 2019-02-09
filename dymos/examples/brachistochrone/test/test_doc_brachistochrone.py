fixed derivative error in 1976atm

dymos/examples/brachistochrone/test/test_doc_brachistochrone.py
@@ -6,12 +6,6 @@ import unittest
 
 class TestBrachistochroneExample(unittest.TestCase):
 
-    @classmethod
-    def tearDownClass(cls):
-        for filename in ['phase0_sim.db']:
-            if os.path.exists(filename):
-                os.remove(filename)
-
     def test_brachistochrone_for_docs_gauss_lobatto(self):
         import numpy as np
         import matplotlib
@@ -66,7 +60,7 @@ class TestBrachistochroneExample(unittest.TestCase):
         # Generate the explicitly simulated trajectory
         t0 = p['phase0.t_initial']
         tf = t0 + p['phase0.t_duration']
-        exp_out = phase.simulate(times=np.linspace(t0, tf, 50))
+        exp_out = phase.simulate(times=np.linspace(t0, tf, 50), record=False)
 
         fig, ax = plt.subplots()
         fig.suptitle('Brachistochrone Solution')

dymos/examples/brachistochrone/test/test_doc_brachistochrone_explicit.py
@@ -6,12 +6,6 @@ import unittest
 
 class TestBrachistochroneExplicitExample(unittest.TestCase):
 
-    @classmethod
-    def tearDownClass(cls):
-        for filename in ['phase0_sim.db']:
-            if os.path.exists(filename):
-                os.remove(filename)
-
     def test_brachistochrone_for_docs_single_shooting(self):
         import numpy as np
         import matplotlib
@@ -76,7 +70,7 @@ class TestBrachistochroneExplicitExample(unittest.TestCase):
         # Generate the explicitly simulated trajectory
         t0 = p['phase0.t_initial']
         tf = t0 + p['phase0.t_duration']
-        exp_out = phase.simulate(times=np.linspace(t0, tf, 50))
+        exp_out = phase.simulate(times=np.linspace(t0, tf, 50), record=False)
 
         fig, ax = plt.subplots()
         fig.suptitle('Brachistochrone Solution')
@@ -161,7 +155,7 @@ class TestBrachistochroneExplicitExample(unittest.TestCase):
         # Generate the explicitly simulated trajectory
         t0 = p['phase0.t_initial']
         tf = t0 + p['phase0.t_duration']
-        exp_out = phase.simulate(times=np.linspace(t0, tf, 50))
+        exp_out = phase.simulate(times=np.linspace(t0, tf, 50), record=False)
 
         fig, ax = plt.subplots()
         fig.suptitle('Brachistochrone Solution')

dymos/models/atmosphere/atmos_1976.py
@@ -130,7 +130,7 @@ T_interp_deriv = T_interp.derivative(1)
 P_interp_deriv = P_interp.derivative(1)
 rho_interp_deriv = rho_interp.derivative(1)
 visc_interp_deriv = visc_interp.derivative(1)
-drho_dh_interp_deriv = rho_interp.derivative(2)
+drho_dh_interp_deriv = rho_interp_deriv.derivative(1)
 
 
 class USatm1976Comp(ExplicitComponent):
@@ -157,26 +157,23 @@ class USatm1976Comp(ExplicitComponent):
         arange = np.arange(nn)
         self.declare_partials(['temp', 'pres', 'rho', 'viscosity', 'drhos_dh', 'sos'], 'h',
                               rows=arange, cols=arange)
-        # self.declare_partials(of='*', wrt='*', method='fd')
 
     def compute(self, inputs, outputs):
-
         outputs['temp'] = T_interp(inputs['h'], extrapolate=True)
         outputs['pres'] = P_interp(inputs['h'], extrapolate=True)
         outputs['rho'] = rho_interp(inputs['h'], extrapolate=True)
         outputs['viscosity'] = visc_interp(inputs['h'], extrapolate=True)
         outputs['drhos_dh'] = rho_interp_deriv(inputs['h'], extrapolate=True)
-
         outputs['sos'] = np.sqrt(self._K*outputs['temp'])
 
     def compute_partials(self, inputs, partials):
         nn = self.options['num_nodes']
         H = inputs['h']
-        partials['temp', 'h'] = T_interp_deriv(H, extrapolate=True).reshape(nn,)[0]
-        partials['pres', 'h'] = P_interp_deriv(H, extrapolate=True).reshape(nn,)[0]
-        partials['rho', 'h'] = rho_interp_deriv(H, extrapolate=True).reshape(nn,)[0]
-        partials['viscosity', 'h'] = visc_interp_deriv(H, extrapolate=True).reshape(nn,)[0]
-        partials['drhos_dh', 'h'] = drho_dh_interp_deriv(H, extrapolate=True).reshape(nn,)[0]
+        partials['temp', 'h'] = T_interp_deriv(H, extrapolate=True)
+        partials['pres', 'h'] = P_interp_deriv(H, extrapolate=True)
+        partials['rho', 'h'] = rho_interp_deriv(H, extrapolate=True)
+        partials['viscosity', 'h'] = visc_interp_deriv(H, extrapolate=True)
+        partials['drhos_dh', 'h'] = drho_dh_interp_deriv(H, extrapolate=True)
 
         T = T_interp(H, extrapolate=True)
-        partials['sos', 'h'] = 0.5/np.sqrt(self._K*T)*partials['temp', 'h']*self._K
+        partials['sos', 'h'] = 0.5/np.sqrt(self._K*T)*partials['temp', 'h'] * self._K