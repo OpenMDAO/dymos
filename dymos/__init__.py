a3a2a0d0af65da69e5f3a596e6a0f77a5621e34c
Rob Falck <robfalck@gmail.com>
2019-04-08 09:17:04 -0400

    Commercial aircraft update (#173)
    * updated commercial aircraft example to new standard
    
    * Bump version:  → 0.13.0
    
    * coverage improvements

--- dymos/__init__.py
+++ dymos/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, division, absolute_import
 
-__version__ = '1.0.0'
+__version__ = '0.13.0'
 
 from .ode_options import ODEOptions, declare_time, declare_state, declare_parameter
 from .phase import Phase
