f8d8510d5dc8f1284e3a6568aa98cc0f05cb2a3b
Rob Falck <rfalck@nasa.gov>
2019-08-19 09:12:08 -0400

    cleaning up shuttle case

--- dymos/examples/shuttle_reentry/atmosphere_comp.py
+++ dymos/examples/shuttle_reentry/atmosphere_comp.py
@@ -1,54 +1,35 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
-from openmdao.api import ExplicitComponent, Problem
+import numpy as np
+import openmdao.api as om
 
-class Atmosphere(ExplicitComponent):
+
+class Atmosphere(om.ExplicitComponent):
+    """
+    Defines the logarithmic atmosphere model for the shuttle reentry problem.
+
+    References
+    ----------
+    .. [1] Betts, John T., Practical Methods for Optimal Control and Estimation Using Nonlinear
+           Programming, p. 248, 2010.
+    """
 
     def initialize(self):
-        self.options.declare("num_nodes", types=int)
+        self.options.declare('num_nodes', types=int)
 
     def setup(self):
-
-        nn = self.options["num_nodes"]
-
-        self.add_input("h", val=np.ones(nn), desc="altitude", units="ft")
-
-        self.add_output("rho", val=np.ones(nn), desc="local density", units="slug/ft**3")
-
+        nn = self.options['num_nodes']
+        self.add_input('h', val=np.ones(nn), desc='altitude', units='ft')
+        self.add_output('rho', val=np.ones(nn), desc='local density', units='slug/ft**3')
         partial_range = np.arange(nn, dtype=int)
-
-        self.declare_partials("rho", "h", rows=partial_range, cols=partial_range)
+        self.declare_partials('rho', 'h', rows=partial_range, cols=partial_range)
 
     def compute(self, inputs, outputs):
-
-        h = inputs["h"]
+        h = inputs['h']
         h_r = 23800
         rho_0 = .002378
+        outputs['rho'] = rho_0 * np.exp(-h / h_r)
 
-        outputs["rho"] = rho_0*np.exp(-h/h_r)
-
-    def compute_partials(self, inputs, J):
-
-        h = inputs["h"]
+    def compute_partials(self, inputs, partials):
+        h = inputs['h']
         h_r = 23800
         rho_0 = .002378
-
-        J["rho", "h"] = -1/(h_r)*rho_0*np.exp(-h/h_r)
-
-
-def test_atmosphere():
-    prob = Problem()
-    prob.model = Atmosphere(num_nodes=5)
-
-    prob.setup(check=False, force_alloc_complex=True)
-
-    prob.run_model()
-
-    return(prob)
-
-if __name__ == "__main__":
-
-    prob = test_atmosphere()
-    prob.check_partials(compact_print=True, method="cs")
-
-    
+        partials['rho', 'h'] = -1 / h_r * rho_0 * np.exp(-h / h_r)
--- dymos/examples/shuttle_reentry/heating_comp.py
+++ dymos/examples/shuttle_reentry/heating_comp.py
@@ -1,73 +1,70 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
-from openmdao.api import ExplicitComponent, Problem
+import numpy as np
+import openmdao.api as om
 
-class AerodynamicHeating(ExplicitComponent):
+
+class AerodynamicHeating(om.ExplicitComponent):
+    """
+    Defines the Aerodynamic heating equations for the shuttle reentry problem.
+
+    References
+    ----------
+    .. [1] Betts, John T., Practical Methods for Optimal Control and Estimation Using Nonlinear
+           Programming, p. 248, 2010.
+    """
 
     def initialize(self):
-        self.options.declare("num_nodes", types=int)
+        self.options.declare('num_nodes', types=int)
 
     def setup(self):
+        nn = self.options['num_nodes']
 
-        nn = self.options["num_nodes"]
+        self.add_input('rho', val=np.ones(nn), desc='local density', units='slug/ft**3')
+        self.add_input('v', val=np.ones(nn), desc='velocity of shuttle', units='ft/s')
+        self.add_input('alpha', val=np.ones(nn), desc='angle of attack of shuttle',
+                       units='deg')
 
-        self.add_input("rho", val=np.ones(nn), desc="local density", units="slug/ft**3")
-        self.add_input("v", val=np.ones(nn), desc="velocity of shuttle", units="ft/s")
-        self.add_input("alpha", val=np.ones(nn), desc="angle of attack of shuttle", 
-                       units="deg")
+        self.add_output('q', val=np.ones(nn),
+                        desc='aerodynamic heating on leading edge of shuttle',
+                        units='Btu/ft**2/s')
 
-        self.add_output("q", val=np.ones(nn), 
-                        desc="aerodynamic heating on leading edge of shuttle", 
-                        units="Btu/ft**2/s")
+        partial_range = np.arange(nn)
 
-        partial_range = np.arange(nn) 
-
-        self.declare_partials("q", "rho", rows=partial_range, cols=partial_range)
-        self.declare_partials("q", "v", rows=partial_range, cols=partial_range)
-        self.declare_partials("q", "alpha", rows=partial_range, cols=partial_range)
+        self.declare_partials('q', 'rho', rows=partial_range, cols=partial_range)
+        self.declare_partials('q', 'v', rows=partial_range, cols=partial_range)
+        self.declare_partials('q', 'alpha', rows=partial_range, cols=partial_range)
 
     def compute(self, inputs, outputs):
+        rho = inputs['rho']
+        v = inputs['v']
+        alpha = inputs['alpha']
+        c_0 = 1.0672181
+        c_1 = -0.19213774e-1
+        c_2 = 0.21286289e-3
+        c_3 = -0.10117249e-5
+        q_r = 17700.0 * np.sqrt(rho) * (0.0001 * v) ** 3.07
+        q_a = c_0 + c_1 * alpha + c_2 * alpha ** 2 + c_3 * alpha ** 3
 
-        rho = inputs["rho"]
-        v = inputs["v"]
-        alpha = inputs["alpha"]
+        outputs['q'] = q_r * q_a
+
+    def compute_partials(self, inputs, partials):
+        rho = inputs['rho']
+        v = inputs['v']
+        alpha = inputs['alpha']
         c_0 = 1.0672181
         c_1 = -.19213774e-1
         c_2 = .21286289e-3
         c_3 = -.10117249e-5
-        q_r = 17700*rho**.5*(.0001*v)**3.07
-        q_a = c_0 + c_1*alpha + c_2*alpha**2 + c_3*alpha**3
 
-        outputs["q"] = q_r*q_a
+        sqrt_rho = np.sqrt(rho)
 
-    def compute_partials(self, inputs, J):
-        
-        rho = inputs["rho"]
-        v = inputs["v"]
-        alpha = inputs["alpha"]
-        c_0 = 1.0672181
-        c_1 = -.19213774e-1
-        c_2 = .21286289e-3
-        c_3 = -.10117249e-5
+        q_r = 17700 * sqrt_rho * (.0001 * v) ** 3.07
+        q_a = c_0 + c_1 * alpha + c_2 * alpha ** 2 + c_3 * alpha ** 3
 
-        q_r = 17700*rho**.5*(.0001*v)**3.07
-        q_a = c_0 + c_1*alpha + c_2*alpha**2 + c_3*alpha**3
+        dqr_drho = 4.644546023210496e-09 * v ** 3.07 / sqrt_rho
+        dqr_dv = 17700 * sqrt_rho * 0.0001 * 3.07 * (0.0001 * v) ** 2.07
 
-        J["q", "rho"] = q_a*.5*17700*rho**(-.5)*(.0001*v)**3.07
-        J["q", "v"] = q_a*3.07*17700*rho**.5*(.0001)**3.07*v**2.07
-        J["q", "alpha"] = q_r*(c_1 + 2*c_2*alpha + 3*c_3*alpha**2)
+        dqa_dalpha = c_1 + 2 * c_2 * alpha + 3 * c_3 * alpha ** 2
 
-def test_heating():
-    prob = Problem()
-    prob.model = AerodynamicHeating(num_nodes=5)
-
-    prob.setup(check=False, force_alloc_complex=True)
-
-    prob.run_model()
-
-    return(prob)
-
-if __name__ == "__main__":
-
-    prob = test_heating()
-    prob.check_partials(compact_print=True, method="cs")
+        partials['q', 'rho'] = dqr_drho * q_a
+        partials['q', 'v'] = dqr_dv * q_a
+        partials['q', 'alpha'] = dqa_dalpha * q_r
