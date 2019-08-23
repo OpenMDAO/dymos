1d5e1be33ebf7afc929221be053ab7bd2cdd6881
Kaushik Ponnapalli <kaushik.s.ponnapalli@nasa.gov>
2019-08-23 09:56:50 -0400

    Fixed code formatting on test problem

--- dymos/examples/hyper_sensitive/hyper_sensitive_ode.py
+++ dymos/examples/hyper_sensitive/hyper_sensitive_ode.py
@@ -3,10 +3,10 @@ from __future__ import print_function, division, absolute_import
 import numpy as np
 import openmdao.api as om
 
-class HyperSensitiveODE(om.ExplicitComponent):
 
-    states = {'x': {'rate_source': 'xdot',
-                    'units': 'm'}}
+class HyperSensitiveODE(om.ExplicitComponent):
+    states = {'x': {'rate_source': 'x_dot'},
+              'xL': {'rate_source': 'L'}}
 
     parameters = {'u': {'targets': 'u'}}
 
@@ -18,26 +18,30 @@ class HyperSensitiveODE(om.ExplicitComponent):
 
         # inputs
         self.add_input('x', val=np.zeros(nn), desc='state')
+        self.add_input('xL', val=np.zeros(nn), desc='cost_state')
 
         self.add_input('u', val=np.zeros(nn), desc='control')
 
         self.add_output('x_dot', val=np.zeros(nn), desc='state rate')
+        self.add_output('L', val=np.zeros(nn), desc='Lagrangian')
 
         # Setup partials
-        self.declare_partials(of='x_dot', wrt='x', rows=np.arange(nn), cols=np.arange(nn))
+        self.declare_partials(of='x_dot', wrt='x', rows=np.arange(nn), cols=np.arange(nn), val=-1)
+        self.declare_partials(of='x_dot', wrt='u', rows=np.arange(nn), cols=np.arange(nn), val=1)
 
-        self.declare_partials(of='x_dot', wrt='u', rows=np.arange(nn), cols=np.arange(nn))
+        self.declare_partials(of='L', wrt='x', rows=np.arange(nn), cols=np.arange(nn))
+        self.declare_partials(of='L', wrt='u', rows=np.arange(nn), cols=np.arange(nn))
 
     def compute(self, inputs, outputs):
         x = inputs['x']
         u = inputs['u']
 
         outputs['x_dot'] = -x + u
+        outputs['L'] = (x ** 2 + u ** 2) / 2
 
     def compute_partials(self, inputs, jacobian):
         x = inputs['x']
         u = inputs['u']
 
-        jacobian['x_dot', 'x'] = -1
-        jacobian['x_dot', 'u'] = 1
-
+        jacobian['L', 'x'] = x
+        jacobian['L', 'u'] = u
