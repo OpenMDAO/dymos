ce2b56eb3b4eb42a4c38bdea6343814943839fe9
Kaushik Ponnapalli <kaushik.s.ponnapalli@nasa.gov>
2019-10-01 12:04:07 -0400

    Added cleaned up grid refinement

---
+++ dymos/run_problem.py
@@ -0,0 +1,139 @@
+from .grid_refinement.ph_adaptive.ph_adaptive import PHAdaptive
+from .phase.phase import Phase
+
+import numpy as np
+import openmdao.api as om
+import os
+
+
+def run_problem(problem, refine=True):
+    problem.run_driver()
+
+    if refine:
+        out_file = 'grid_refinement.out'
+
+        phases = {phase_path: problem.model._get_subsystem(phase_path)
+                  for phase_path in find_phases(problem.model)}
+
+        ph = PHAdaptive(phases, iteration_limit=20, tol=1e-4)
+        f = open(out_file, 'w+')
+        write_initial(f, ph.min_order, ph.max_order, ph.iteration_limit, ph.tol)
+
+        for i in range(ph.iteration_limit):
+            write_iteration(f, i, phases)
+            need_refine = ph.check_error()
+
+            if all(refine_segment is False for refine_segment in need_refine.values()):
+                break
+
+            prev_soln = {'inputs': problem.model.list_inputs(out_stream=None, units=True),
+                         'outputs': problem.model.list_outputs(out_stream=None, units=True)}
+
+            refined_phases = ph.refine(need_refine)
+            if not refined_phases:
+                break
+
+            problem.setup()
+
+            re_interpolate_solution(problem, phases, previous_solution=prev_soln)
+
+            problem.run_driver()
+        if i == ph.iteration_limit-1:
+            f.write('\nIteration limit exceeded. Unable to satisfy specified tolerance')
+        else:
+            f.write('\nSuccessfully completed grid refinement')
+        f.close()
+
+
+def find_phases(sys):
+    phase_paths = []
+    if isinstance(sys, Phase):
+        phase_paths.append(sys.pathname)
+    elif isinstance(sys, om.Group):
+        for subsys in sys._loc_subsys_map:
+            phase_paths.extend(find_phases(getattr(sys, subsys)))
+    return phase_paths
+
+
+def re_interpolate_solution(problem, phases, previous_solution):
+
+    phase_paths = phases.keys()
+
+    prev_ip_dict = {k: v['value'] for k, v in previous_solution['inputs']}
+    prev_op_dict = {k: v['value'] for k, v in previous_solution['outputs']}
+
+    abs_to_prom_ip_map = {}
+    for prom_name, abs_names in problem.model._var_allprocs_prom2abs_list['input'].items():
+        for abs_name in abs_names:
+            abs_to_prom_ip_map[abs_name] = prom_name
+
+    abs_to_prom_op_map = {}
+    for prom_name, abs_names in problem.model._var_allprocs_prom2abs_list['output'].items():
+        for abs_name in abs_names:
+            abs_to_prom_op_map[abs_name] = prom_name
+
+    for phase_path, phase in phases.items():
+        prom_to_abs_ip_map = phase._var_allprocs_prom2abs_list['input']
+        prom_to_abs_op_map = phase._var_allprocs_prom2abs_list['output']
+
+        ti_abs_name = prom_to_abs_op_map['t_initial'][0]
+        ti_prom_name = abs_to_prom_op_map[f'{phase_path}.time_extents.t_initial']
+        t_initial = prev_op_dict[ti_abs_name]
+
+        td_abs_name = prom_to_abs_op_map['t_duration'][0]
+        td_prom_name = abs_to_prom_op_map[f'{phase_path}.time_extents.t_duration']
+        t_duration = prev_op_dict[td_abs_name]
+
+        prev_time = prev_op_dict[f'{phase_path}.time.time']
+
+        problem.set_val(ti_prom_name, t_initial)
+        problem.set_val(td_prom_name, t_duration)
+
+        for state_name, options in phase.state_options.items():
+            state_abs_name = f'{phase_path}.indep_states.states:{state_name}'
+            prev_state_soln_abs_name = f'{phase_path}.timeseries.states:{state_name}'
+            state_prom_name = abs_to_prom_op_map[state_abs_name]
+            prev_state_val = prev_op_dict[prev_state_soln_abs_name]
+            problem.set_val(state_prom_name,
+                            phase.interpolate(xs=prev_time, ys=prev_state_val, nodes='state_input', kind='slinear'))
+
+        for control_name, options in phase.control_options.items():
+            control_abs_name = f'{phase_path}.control_group.indep_controls.controls:{control_name}'
+            prev_control_soln_abs_name = f'{phase_path}.timeseries.controls:{control_name}'
+            control_prom_name = abs_to_prom_op_map[control_abs_name]
+            prev_control_val = prev_op_dict[prev_control_soln_abs_name]
+            problem.set_val(control_prom_name,
+                            phase.interpolate(xs=prev_time, ys=prev_control_val, nodes='control_input', kind='slinear'))
+
+
+def write_initial(f, min, max, iter_limit, tol):
+    f.write('======================\n')
+    f.write('   Grid Refinement\n')
+    f.write('======================\n')
+    f.write('ph-refinement\n')
+    f.write('Minimum order = {}\n'.format(min))
+    f.write('Maximum order = {}\n'.format(max))
+    f.write('Tolerance = {}\n'.format(tol))
+    f.write('Iteration limit = {}\n'.format(iter_limit))
+
+
+def write_iteration(f, iter_number, phases):
+    f.write('\n\n')
+    f.write('Iteration number: {}\n'.format(iter_number))
+    for phase_path, phase in phases.items():
+        f.write('Phase: {}\n'.format(phase_path))
+        T = phase.options['transcription']
+        gd = phase.options['transcription'].grid_data
+
+        f.write('Number of Segments = {}\n'.format(T.options['num_segments']))
+
+        f.write('Segment Ends = [')
+        f.write(', '.join(str(round(elem, 4)) for elem in gd.segment_ends))
+        f.write(']\n')
+
+        if isinstance(T.options['order'], int):
+            f.write('Segment Order = {}\n'.format(T.options['order']))
+        else:
+            f.write('Segment Order = [')
+            f.write(', '.join(str(elem) for elem in T.options['order']))
+            f.write(']\n')
---
+++ dymos/test/test_run_problem.py
@@ -0,0 +1,114 @@
+import unittest
+import openmdao.api as om
+from openmdao.utils.assert_utils import assert_rel_error
+import dymos as dm
+import numpy as np
+
+from dymos.examples.hyper_sensitive.hyper_sensitive_ode import HyperSensitiveODE
+from dymos.examples.brachistochrone.brachistochrone_ode import BrachistochroneODE
+
+from openmdao.utils.general_utils import set_pyoptsparse_opt
+
+
+class TestRunProblem(unittest.TestCase):
+
+    def test_run_HS_problem(self):
+        p = om.Problem(model=om.Group())
+        p.driver = om.pyOptSparseDriver()
+        p.driver.declare_coloring()
+        _, optimizer = set_pyoptsparse_opt('SNOPT', fallback=True)
+        p.driver.options['optimizer'] = optimizer
+
+        traj = p.model.add_subsystem('traj', dm.Trajectory())
+        phase0 = traj.add_phase('phase0', dm.Phase(ode_class=HyperSensitiveODE,
+                                                   transcription=dm.Radau(num_segments=30, order=3)))
+        phase0.set_time_options(fix_initial=True, fix_duration=True)
+        phase0.add_state('x', fix_initial=True, fix_final=False, rate_source='x_dot', targets=['x'])
+        phase0.add_state('xL', fix_initial=True, fix_final=False, rate_source='L', targets=['xL'])
+        phase0.add_control('u', opt=True, targets=['u'], rate_continuity=False)
+
+        phase0.add_boundary_constraint('x', loc='final', equals=1)
+
+        phase0.add_objective('xL', loc='final')
+
+        p.setup(check=True)
+
+        tf = 100
+
+        p.set_val('traj.phase0.states:x', phase0.interpolate(ys=[1.5, 1], nodes='state_input'))
+        p.set_val('traj.phase0.states:xL', phase0.interpolate(ys=[0, 1], nodes='state_input'))
+        p.set_val('traj.phase0.t_initial', 0)
+        p.set_val('traj.phase0.t_duration', tf)
+        p.set_val('traj.phase0.controls:u', phase0.interpolate(ys=[-0.6, 2.4],
+                                                               nodes='control_input'))
+        dm.run_problem(p, True)
+
+        sqrt_two = np.sqrt(2)
+        val = sqrt_two * tf
+        c1 = (1.5 * np.exp(-val) - 1) / (np.exp(-val) - np.exp(val))
+        c2 = (1 - 1.5 * np.exp(val)) / (np.exp(-val) - np.exp(val))
+
+        ui = c1 * (1 + sqrt_two) + c2 * (1 - sqrt_two)
+        uf = c1 * (1 + sqrt_two) * np.exp(val) + c2 * (1 - sqrt_two) * np.exp(-val)
+        J = 0.5 * (c1 ** 2 * (1 + sqrt_two) * np.exp(2 * val) + c2 ** 2 * (1 - sqrt_two) * np.exp(-2 * val) -
+                   (1 + sqrt_two) * c1 ** 2 - (1 - sqrt_two) * c2 ** 2)
+
+        assert_rel_error(self,
+                         p.get_val('traj.phase0.timeseries.controls:u')[0],
+                         ui,
+                         tolerance=1e-4)
+
+        assert_rel_error(self,
+                         p.get_val('traj.phase0.timeseries.controls:u')[-1],
+                         uf,
+                         tolerance=1e-4)
+
+        assert_rel_error(self,
+                         p.get_val('traj.phase0.timeseries.states:xL')[-1],
+                         J,
+                         tolerance=1e-4)
+
+    def test_run_brachistochrone_problem(self):
+        p = om.Problem(model=om.Group())
+        p.driver = om.pyOptSparseDriver()
+        p.driver.declare_coloring()
+        p.driver.options['optimizer'] = 'SLSQP'
+
+        traj = p.model.add_subsystem('traj', dm.Trajectory())
+        phase0 = traj.add_phase('phase0', dm.Phase(ode_class=BrachistochroneODE,
+                                                   transcription=dm.Radau(num_segments=10, order=3)))
+        phase0.set_time_options(fix_initial=True, fix_duration=True)
+        phase0.add_state('x', rate_source=BrachistochroneODE.states['x']['rate_source'],
+                         units=BrachistochroneODE.states['x']['units'],
+                         fix_initial=True, fix_final=False, solve_segments=False)
+        phase0.add_state('y', rate_source=BrachistochroneODE.states['y']['rate_source'],
+                         units=BrachistochroneODE.states['y']['units'],
+                         fix_initial=True, fix_final=False, solve_segments=False)
+        phase0.add_state('v', rate_source=BrachistochroneODE.states['v']['rate_source'],
+                         targets=BrachistochroneODE.states['v']['targets'],
+                         units=BrachistochroneODE.states['v']['units'],
+                         fix_initial=True, fix_final=False, solve_segments=False)
+        phase0.add_control('theta', targets=BrachistochroneODE.parameters['theta']['targets'],
+                           continuity=True, rate_continuity=True,
+                           units='deg', lower=0.01, upper=179.9)
+        phase0.add_input_parameter('g', targets=BrachistochroneODE.parameters['g']['targets'],
+                                   units='m/s**2', val=9.80665)
+
+        phase0.add_boundary_constraint('x', loc='final', equals=10)
+        phase0.add_boundary_constraint('y', loc='final', equals=5)
+        # Minimize time at the end of the phase
+        phase0.add_objective('time_phase', loc='final', scaler=10)
+
+        p.model.linear_solver = om.DirectSolver()
+        p.setup(check=True)
+
+        p.set_val('traj.phase0.t_initial', 0.0)
+        p.set_val('traj.phase0.t_duration', 2.0)
+
+        p.set_val('traj.phase0.states:x', phase0.interpolate(ys=[0, 10], nodes='state_input'))
+        p.set_val('traj.phase0.states:y', phase0.interpolate(ys=[10, 5], nodes='state_input'))
+        p.set_val('traj.phase0.states:v', phase0.interpolate(ys=[0, 9.9], nodes='state_input'))
+        p.set_val('traj.phase0.controls:theta', phase0.interpolate(ys=[5, 100], nodes='control_input'))
+        p.set_val('traj.phase0.input_parameters:g', 9.80665)
+
+        dm.run_problem(p, True)
