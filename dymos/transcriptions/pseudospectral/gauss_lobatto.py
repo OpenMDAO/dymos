Fixed units and variable name errors

dymos/transcriptions/pseudospectral/gauss_lobatto.py
@@ -223,7 +223,7 @@ class GaussLobatto(PseudospectralBase):
             shape = options['shape']
             units = options['units']
             interleave_comp.add_var('states:{0}'.format(state_name), shape, units)
-            interleave_comp.add_var('state_rates:{0}'.format(options['rate_source']), shape, units)
+            interleave_comp.add_var('state_rates:{0}'.format(state_name), shape, units)
 
             size = np.prod(options['shape'])
             src_idxs_mat = np.reshape(np.arange(size * num_input_nodes, dtype=int),
@@ -474,12 +474,12 @@ class GaussLobatto(PseudospectralBase):
                 phase.connect(src_name='interleave_comp.all_values:states:{0}'.format(state_name),
                               tgt_name='{0}.input_values:states:{1}'.format(name, state_name))
 
-                timeseries_comp._add_timeseries_output('state_rates:{0}'.format(options['rate_source']),
+                timeseries_comp._add_timeseries_output('state_rates:{0}'.format(state_name),
                                                        var_class=phase.classify_var(options['rate_source']),
                                                        shape=options['shape'],
-                                                       units=options['units'])
-                phase.connect(src_name='interleave_comp.all_values:state_rates:{0}'.format(options['rate_source']),
-                              tgt_name='{0}.input_values:state_rates:{1}'.format(name, options['rate_source']))
+                                                       units=get_rate_units(options['units'], time_units))
+                phase.connect(src_name='interleave_comp.all_values:state_rates:{0}'.format(state_name),
+                              tgt_name='{0}.input_values:state_rates:{1}'.format(name, state_name))
 
             for control_name, options in iteritems(phase.control_options):
                 control_units = options['units']