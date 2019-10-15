52d3a5946b5a1af9216ae3a71b43392e2f55146e
Rob Falck <robfalck@gmail.com>
2019-10-15 13:04:12 -0400

    Load case (#236)
    * Dymos now requires OpenMDAO version 2.9.0 or later. - Resolves #232
    Increased default maxiter for the NewtonSolver when using Pseudospectral transcription with solve_segments=True. - Resolves #234
    Added a load_case functionality that will interpolate a previous solution from a CaseReader when the grid has changed. - Resolves #233
    
    The load_case functionality is currently experimental and requires OpenMDAO master as of October 07, 2019 or later.
    
    * pep8 fixes

--- dymos/__init__.py
+++ dymos/__init__.py
@@ -7,3 +7,4 @@ from .phase import Phase
 from .transcriptions import GaussLobatto, Radau, RungeKutta
 from .trajectory.trajectory import Trajectory
 from .run_problem import run_problem
+from .load_case import load_case
