85d49dcba363bd952d4460520d8adcbda9dde514
Rob Falck <rfalck@nasa.gov>
2020-02-18 11:32:58 -0500

    added brachistochrone test case for command line and the start of some test assertions for the CLI

--- dymos/utils/test/test_command_line.py
+++ dymos/utils/test/test_command_line.py
@@ -3,6 +3,7 @@ import dymos.utils.command_line as command_line
 from openmdao.utils.testing_utils import use_tempdirs
 import sys
 import os
+import openmdao.api as om
 
 
 # TODO: these tests currently just check that the problem runs, they should check results
@@ -18,16 +19,45 @@ class TestCommandLine(unittest.TestCase):
             except FileNotFoundError:
                 pass  # OK if old database is not present to be deleted
 
+    def _assert_correct_solution(self):
+        # Fail if the recorded driver solution file does not exist (driver did not execute)
+        self.failUnless(os.path.exists('dymos_solution.db'), 'solution database was not recorded')
+
+        # Assert the results are what we expect.
+        cr = om.CaseReader('dymos_solution.db')
+
+        # Make sure there are cases
+        num_cases = len(cr.list_cases())
+        self.assertTrue(num_cases > 0)
+        # If there are cases, get the last one
+        case = cr.get_case(-1)
+
+        # Make sure the driver converged
+        self.assertTrue(case.success)
+
+
     def test_ex_brachistochrone_stock(self):
+        """ Test to verify that the command line interface intercepts final_setup and runs
+        dm.run_problem by default without any additional arguments. """
         print('test_ex_brachistochrone_stock')
         sys.argv = ['dymos',
-                    self.test_dir + '/../../examples/brachistochrone/test/ex_brachistochrone.py']
+                    os.path.join(self.test_dir, 'brachistochrone_for_command_line.py')]
+        command_line.dymos_cmd()
+        self._assert_correct_solution()
+
+    def test_ex_brachistochrone_stock_nosolve_nosim(self):
+        """ Test to verify that the command line interface intercepts final_setup and
+        does nothing if given `--no_solve` and not given `--simulate`. """
+        print('test_ex_brachistochrone_stock')
+        sys.argv = ['dymos',
+                    os.path.join(self.test_dir, 'brachistochrone_for_command_line.py'),
+                    '--no_solve']
         command_line.dymos_cmd()
 
     def test_ex_brachistochrone_iteration(self):
         print('test_ex_brachistochrone_iteration')
         sys.argv = ['dymos',
-                    self.test_dir + '/../../examples/brachistochrone/test/ex_brachistochrone.py',
+                    os.path.join(self.test_dir, 'brachistochrone_for_command_line.py'),
                     '--refine_limit=5']
         command_line.dymos_cmd()
 
@@ -35,34 +65,34 @@ class TestCommandLine(unittest.TestCase):
         # run stock problem first to record the output database
         print('test_ex_brachistochrone_solution first run')
         sys.argv = ['dymos',
-                    self.test_dir + '/../../examples/brachistochrone/test/ex_brachistochrone.py']
+                    os.path.join(self.test_dir, 'brachistochrone_for_command_line.py')]
         command_line.dymos_cmd()
 
         # run problem again loading the output database
         print('test_ex_brachistochrone_solution second run')
         sys.argv = ['dymos',
-                    self.test_dir + '/../../examples/brachistochrone/test/ex_brachistochrone.py',
+                    os.path.join(self.test_dir, 'brachistochrone_for_command_line.py'),
                     '--solution=dymos_solution.db']
         command_line.dymos_cmd()
 
     def test_ex_brachistochrone_no_solve(self):
         print('test_ex_brachistochrone_no_solve')
         sys.argv = ['dymos',
-                    self.test_dir + '/../../examples/brachistochrone/test/ex_brachistochrone.py',
+                    os.path.join(self.test_dir, 'brachistochrone_for_command_line.py'),
                     '--no_solve']
         command_line.dymos_cmd()
 
     def test_ex_brachistochrone_simulate(self):
         print('test_ex_brachistochrone_simulate')
         sys.argv = ['dymos',
-                    self.test_dir + '/../../examples/brachistochrone/test/ex_brachistochrone.py',
+                    os.path.join(self.test_dir, 'brachistochrone_for_command_line.py'),
                     '--simulate']
         command_line.dymos_cmd()
 
     def test_ex_brachistochrone_reset_grid(self):
         print('test_ex_brachistochrone_reset_grid')
         sys.argv = ['dymos',
-                    self.test_dir + '/../../examples/brachistochrone/test/ex_brachistochrone.py',
+                    os.path.join(self.test_dir, 'brachistochrone_for_command_line.py'),
                     '--reset_grid']
         command_line.dymos_cmd()
 
