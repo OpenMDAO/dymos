5deddbf68b4b33c1bb42bce9d09ebb3862111a20
Ted Wright <ted.wright@nasa.gov>
2020-02-25 13:05:00 -0500

    add vanderpol example problem

---
+++ dymos/examples/vanderpol/test/test_vanderpol.py
@@ -0,0 +1,49 @@
+import unittest
+from numpy.testing import assert_almost_equal
+import numpy as np
+import dymos as dm
+from dymos.examples.vanderpol.vanderpol_dymos import vanderpol
+from dymos.examples.vanderpol.vanderpol_dymos_plots import vanderpol_dymos_plots
+from openmdao.utils.testing_utils import use_tempdirs
+
+SHOW_PLOTS = False
+
+
+@use_tempdirs
+class TestVanderpolExample(unittest.TestCase):
+    def test_vanderpol_simulate(self):
+        # simulate only: with no control, the system oscillates
+        p = vanderpol(transcription='gauss-lobatto', num_segments=75)
+        p.run_model()
+
+        if SHOW_PLOTS:
+            vanderpol_dymos_plots(p)
+
+    def test_vanderpol_optimal(self):
+        p = vanderpol(transcription='gauss-lobatto', num_segments=75)
+        dm.run_problem(p)  # find optimal control solution to stop oscillation
+
+        if SHOW_PLOTS:
+            vanderpol_dymos_plots(p)
+
+        print('Objective function minimized to', p['traj.phases.phase0.final_conditions.states:J++'])
+        # check that ODE states (excluding J) and control are driven to near zero
+        assert_almost_equal(p['traj.phases.phase0.final_conditions.states:x0++'], np.zeros(1))
+        assert_almost_equal(p['traj.phases.phase0.final_conditions.states:x1++'], np.zeros(1))
+        assert_almost_equal(p['traj.phases.phase0.final_conditions.controls:u++'], np.zeros(1), decimal=3)
+
+    def test_vanderpol_optimal_grid_refinement(self):
+        # enabling grid refinement gives a faster and better solution with fewer segments
+        p = vanderpol(transcription='gauss-lobatto', num_segments=15)
+
+        p.model.traj.phases.phase0.set_refine_options(refine=True)
+        dm.run_problem(p, refine=True, refine_iteration_limit=10)  # enable grid refinement and find optimal solution
+
+        if SHOW_PLOTS:
+            vanderpol_dymos_plots(p)
+
+        print('Objective function minimized to', p['traj.phases.phase0.final_conditions.states:J++'])
+        # check that ODE states (excluding J) and control are driven to near zero
+        assert_almost_equal(p['traj.phases.phase0.final_conditions.states:x0++'], np.zeros(1))
+        assert_almost_equal(p['traj.phases.phase0.final_conditions.states:x1++'], np.zeros(1))
+        assert_almost_equal(p['traj.phases.phase0.final_conditions.controls:u++'], np.zeros(1), decimal=4)
