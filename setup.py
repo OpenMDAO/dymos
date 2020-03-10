4db8ffe021cd64ed505d4587559b7d311f6e7144
Rob Falck <rfalck@nasa.gov>
2020-03-10 11:11:59 -0400

    run testflo on site-packages/dymos from original directory

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(name='dymos',
     license='Apache License',
     packages=find_packages(),
     install_requires=[
-        'openmdao>=2.10.1',
+        'openmdao>=3.0.0',
         'numpy>=1.14.1',
         'scipy>=1.0.0',
         'pep8',
