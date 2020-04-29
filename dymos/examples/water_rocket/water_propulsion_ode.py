ac4e2b5dd60f69d2bb9c3e92530d7eea2da5ba25
Bernardo Bahia Monteiro <bbahia@umich.edu>
2020-04-28 20:49:02 -0400

    Add component to calculate total mass

--- dymos/examples/water_rocket/water_propulsion_ode.py
+++ dymos/examples/water_rocket/water_propulsion_ode.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import openmdao.api as om
 from dymos.examples.min_time_climb.aero.dynamic_pressure_comp import DynamicPressureComp
 from dymos.examples.min_time_climb.aero.lift_drag_force_comp import LiftDragForceComp
@@ -17,6 +19,9 @@ class WaterPropulsionODE(om.Group):
         self.add_subsystem(name='water_engine',
                            subsys=WaterEngine(num_nodes=nn))
 
+        self.add_subsystem(name='mass_adder',
+                           subsys=_MassAdder(num_nodes=nn))
+
         self.add_subsystem(name='atmos',
                            subsys=USatm1976Comp(num_nodes=nn))
 
@@ -35,6 +40,30 @@ class WaterPropulsionODE(om.Group):
         self.connect('aero.f_drag', 'eom.D')
         self.connect('aero.f_lift', 'eom.L')
         self.connect('water_engine.F', 'eom.T')
+        self.connect('mass_adder.m', 'eom.m')
+
+
+class _MassAdder(om.ExplicitComponent):
+
+    def initialize(self):
+        self.options.declare('num_nodes', types=int)
+
+    def setup(self):
+        nn = self.options['num_nodes']
+
+        self.add_input('m_empty', val=np.zeros(nn), desc='empty mass', units='kg')
+        self.add_input('V_w', val=np.zeros(nn), desc='water volume', units='m**3')
+        self.add_input('rho_w', val=1e3*np.ones(nn), desc="water density", units='kg/m**3')
+
+        self.add_output('m', val=np.zeros(nn), desc='total mass', units='kg')
+
+    def compute(self, inputs, outputs):
+        outputs['m'] = inputs['m_empty'] + inputs['rho_w']*inputs['V_w']
+
+    def compute_partials(self, inputs, jacobian):
+        jacobian['m', 'm_empty'] = 1
+        jacobian['m', 'rho_w'] = inputs['V_w']
+        jacobian['m', 'V_w'] = inputs['rho_w']
 
 
 if __name__=='__main__':
