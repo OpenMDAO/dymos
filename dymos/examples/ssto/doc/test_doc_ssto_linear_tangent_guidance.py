6b4c6de201cc78337b7a08f2b42443b02139d838
Ted Wright <ted.wright@nasa.gov>
2020-06-16 09:22:02 -0400

    add ssto lunar linear tangent example problem

--- dymos/examples/ssto/doc/test_doc_ssto_linear_tangent_guidance.py
+++ dymos/examples/ssto/doc/test_doc_ssto_linear_tangent_guidance.py
@@ -1,4 +1,5 @@
 import unittest
+from dymos.utils.doc_utils import save_for_docs
 
 import matplotlib
 matplotlib.use('Agg')
@@ -7,7 +8,7 @@ plt.style.use('ggplot')
 
 
 class TestDocSSTOLinearTangentGuidance(unittest.TestCase):
-
+    @save_for_docs
     def test_doc_ssto_linear_tangent_guidance(self):
         import numpy as np
         import matplotlib.pyplot as plt
