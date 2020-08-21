3a0ed60182f3d8b80b9c56c8f0d787cba2a43ba8
Rob Falck <rfalck@nasa.gov>
2020-08-21 14:25:54 -0400

    Bump version: 0.15.0 → 0.15.1

--- dymos/__init__.py
+++ dymos/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.15.0'
+__version__ = '0.15.1'
 
 from .phase import Phase
 from .transcriptions import GaussLobatto, Radau, RungeKutta
--- dymos/docs/conf.py
+++ dymos/docs/conf.py
@@ -102,7 +102,7 @@ author = u'Rob Falck and John Hwang'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.15.0'
+version = u'0.15.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
