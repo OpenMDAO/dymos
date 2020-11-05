b1098685e7512008192fb71a3e85f706d96a1344
Rob Falck <rfalck@nasa.gov>
2020-11-05 14:54:50 -0500

    gauss lobatto and radau now work with matrix states, working on simulation

--- dymos/examples/cannonball/test/test_cannonball_matrix_state.py
+++ dymos/examples/cannonball/test/test_cannonball_matrix_state.py
@@ -3,6 +3,7 @@ import openmdao.api as om
 import dymos as dm
 
 import unittest
+from openmdao.utils.assert_utils import assert_near_equal
 
 
 class ODEComp(om.ExplicitComponent):
@@ -28,24 +29,52 @@ class ODEComp(om.ExplicitComponent):
 
 class TestCannonballMatrixState(unittest.TestCase):
     """ Tests to verify that dymos can use matrix-states"""
-
-    def test_cannonball_matrix_state(self):
+    def _make_problem(self, tx):
         p = om.Problem()
 
         traj = dm.Trajectory()
         phase = dm.Phase(ode_class=ODEComp,
-                         transcription=dm.Radau(num_segments=10, order=3, solve_segments=True))
+                         transcription=tx)
         traj.add_phase('phase', phase)
         p.model.add_subsystem('traj', traj)
 
-        phase.set_time_options(fix_initial=True, duration_bounds=(5, 5))
+        phase.set_time_options(fix_initial=True, duration_bounds=(1, 5))
         phase.add_state('z', rate_source='zdot', fix_initial=True, units=None)
+
+        phase.add_boundary_constraint('z', loc='final', lower=0, upper=0, indices=[1])
+        phase.add_objective('time', loc='final')
+
+        p.driver = om.pyOptSparseDriver()
 
         p.setup()
 
+        p.set_val('traj.phase.t_initial', 0)
+        p.set_val('traj.phase.t_duration', 5)
         p.set_val('traj.phase.states:z', phase.interpolate(ys=[[[0, 0], [10, 10]], [[10, 0], [10, -10]]], nodes='state_input'))
 
-        p.run_model()
+        return p
 
-        p.model.list_outputs(print_arrays=True, residuals=True)
+    def test_cannonball_matrix_state_radau(self):
 
+        tx = dm.Radau(num_segments=10, order=3, solve_segments=True)
+
+        p = self._make_problem(tx)
+
+        p.run_driver()
+
+        assert_near_equal(p.get_val('traj.phase.timeseries.time')[-1], 2.03873598, tolerance=1E-5)
+        assert_near_equal(p.get_val('traj.phase.timeseries.states:z')[-1, 0, 1], 0.0, tolerance=1E-5)
+        assert_near_equal(p.get_val('traj.phase.timeseries.states:z')[-1, 0, 0], 20.3873598, tolerance=1E-5)
+
+        p.model.traj.simulate()
+
+    def test_cannonball_matrix_state_gl(self):
+        tx = dm.GaussLobatto(num_segments=10, order=3, solve_segments=True)
+
+        p = self._make_problem(tx)
+
+        p.run_driver()
+
+        assert_near_equal(p.get_val('traj.phase.timeseries.time')[-1], 2.03873598, tolerance=1E-5)
+        assert_near_equal(p.get_val('traj.phase.timeseries.states:z')[-1, 0, 1], 0.0, tolerance=1E-5)
+        assert_near_equal(p.get_val('traj.phase.timeseries.states:z')[-1, 0, 0], 20.3873598, tolerance=1E-5)
--- dymos/transcriptions/pseudospectral/radau_pseudospectral.py
+++ dymos/transcriptions/pseudospectral/radau_pseudospectral.py
@@ -145,7 +145,7 @@ class Radau(PseudospectralBase):
 
             phase.connect(rate_src,
                           'collocation_constraint.f_computed:{0}'.format(name),
-                          src_indices=src_idxs, flat_src_indices=True)
+                          src_indices=src_idxs)
 
     def configure_path_constraints(self, phase):
         super(Radau, self).configure_path_constraints(phase)
@@ -255,15 +255,14 @@ class Radau(PseudospectralBase):
                                                       desc=f'rate of state {state_name}')
 
                 src_rows = gd.input_maps['state_input_to_disc']
-                src_idxs = get_src_indices_by_row(src_rows, options['shape'])
                 phase.connect(src_name=f'states:{state_name}',
                               tgt_name=f'{timeseries_name}.input_values:states:{state_name}',
-                              src_indices=src_idxs, flat_src_indices=True)
+                              src_indices=om.slicer[src_rows, ...])
 
                 rate_src, src_idxs = self.get_rate_source_path(state_name, 'all', phase)
                 phase.connect(src_name=rate_src,
                               tgt_name=f'{timeseries_name}.input_values:state_rates:{state_name}',
-                              src_indices=src_idxs, flat_src_indices=True)
+                              src_indices=src_idxs)
 
             for control_name, options in phase.control_options.items():
                 control_units = options['units']
@@ -480,7 +479,7 @@ class Radau(PseudospectralBase):
             rate_path = 'rhs_all.{0}'.format(var)
             node_idxs = gd.subset_node_indices[nodes]
 
-        src_idxs = get_src_indices_by_row(node_idxs, shape=shape)
+        src_idxs = om.slicer[node_idxs, ...]
 
         return rate_path, src_idxs
 
