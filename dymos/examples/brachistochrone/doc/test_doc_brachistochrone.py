f24c9a272e6c711310d90e56b17a49624abfbe2f
Rob Falck <rfalck@nasa.gov>
2020-11-10 12:40:25 -0500

    more include_check_partials cleanup

--- dymos/examples/brachistochrone/doc/test_doc_brachistochrone.py
+++ dymos/examples/brachistochrone/doc/test_doc_brachistochrone.py
@@ -81,15 +81,12 @@ class TestBrachistochroneForDocs(unittest.TestCase):
         phase.set_time_options(initial_bounds=(0, 0), duration_bounds=(.5, 10))
 
         phase.add_state('x', rate_source=BrachistochroneODE.states['x']['rate_source'],
-                        units=BrachistochroneODE.states['x']['units'],
                         fix_initial=True, fix_final=True, solve_segments=False)
 
         phase.add_state('y', rate_source=BrachistochroneODE.states['y']['rate_source'],
-                        units=BrachistochroneODE.states['y']['units'],
                         fix_initial=True, fix_final=True, solve_segments=False)
 
         phase.add_state('v', rate_source=BrachistochroneODE.states['v']['rate_source'],
-                        units=BrachistochroneODE.states['v']['units'],
                         fix_initial=True, fix_final=False, solve_segments=False)
 
         phase.add_control('theta', continuity=True, rate_continuity=True,
