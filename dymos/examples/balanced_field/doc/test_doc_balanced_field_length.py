fabe8733c66701060938de7686814be4f68e29a6
Rob Falck <rfalck@nasa.gov>
2021-02-01 16:36:39 -0500

    moved most parameters to the trajectory level, but restarts with trajectory parameters appears to be broken.

--- dymos/examples/balanced_field/doc/test_doc_balanced_field_length.py
+++ dymos/examples/balanced_field/doc/test_doc_balanced_field_length.py
@@ -60,16 +60,14 @@ br_to_v1 = dm.Phase(ode_class=GroundRollODEComp, transcription=dm.Radau(num_segm
 #
 br_to_v1.set_time_options(fix_initial=True, duration_bounds=(1, 1000), duration_ref=10.0)
 
-br_to_v1.add_state('r', fix_initial=True, lower=0, ref=1000.0, defect_ref=1000.0,
-             rate_source='r_dot')
+br_to_v1.add_state('r', fix_initial=True, lower=0, ref=1000.0, defect_ref=1000.0)
 
-br_to_v1.add_state('v', fix_initial=True, lower=0.0001, ref=100.0, defect_ref=100.0, rate_source='v_dot')
+br_to_v1.add_state('v', fix_initial=True, lower=0.0001, ref=100.0, defect_ref=100.0)
 
-br_to_v1.add_parameter('h', opt=False, units='m', dynamic=True)
-br_to_v1.add_parameter('T', val=27000 * 2, opt=False, units='lbf', dynamic=False)
-br_to_v1.add_parameter('m', val=174200, opt=False, units='lbm')
+# br_to_v1.add_parameter('h', opt=False, units='m', dynamic=False)
+# br_to_v1.add_parameter('T', val=27000 * 2, opt=False, units='lbf', dynamic=False)
 br_to_v1.add_parameter('alpha', val=0.0, opt=False, units='deg')
-br_to_v1.add_parameter('mu_r', val=0.03, opt=False, units=None, dynamic=False)
+# br_to_v1.add_parameter('mu_r', val=0.03, opt=False, units=None, dynamic=False)
 
 br_to_v1.add_timeseries_output('*')
 
@@ -85,16 +83,13 @@ v1_to_vr = dm.Phase(ode_class=GroundRollODEComp, transcription=dm.Radau(num_segm
 #
 v1_to_vr.set_time_options(fix_initial=False, duration_bounds=(1, 1000), duration_ref=1.0)
 
-v1_to_vr.add_state('r', fix_initial=False, lower=0, ref=1000.0, defect_ref=1000.0,
-             rate_source='r_dot')
+v1_to_vr.add_state('r', fix_initial=False, lower=0, ref=1000.0, defect_ref=1000.0)
 
-v1_to_vr.add_state('v', fix_initial=False, lower=0.0001, ref=100.0, defect_ref=100.0,
-             rate_source='v_dot')
+v1_to_vr.add_state('v', fix_initial=False, lower=0.0001, ref=100.0, defect_ref=100.0)
 
-v1_to_vr.add_parameter('h', val=0.0, opt=False, units='m', dynamic=True)
-v1_to_vr.add_parameter('T', val=27000, opt=False, units='lbf', dynamic=False)
-v1_to_vr.add_parameter('m', val=174200, opt=False, units='lbm')
-v1_to_vr.add_parameter('mu_r', val=0.03, opt=False, units=None, dynamic=False)
+# v1_to_vr.add_parameter('h', val=0.0, opt=False, units='m', dynamic=False)
+# v1_to_vr.add_parameter('T', val=27000, opt=False, units='lbf', dynamic=False)
+# v1_to_vr.add_parameter('mu_r', val=0.03, opt=False, units=None, dynamic=False)
 v1_to_vr.add_parameter('alpha', val=0.0, opt=False, units='deg')
 v1_to_vr.add_timeseries_output('*')
 
@@ -107,16 +102,13 @@ rto = dm.Phase(ode_class=GroundRollODEComp, transcription=dm.Radau(num_segments=
 #
 rto.set_time_options(fix_initial=False, duration_bounds=(1, 1000), duration_ref=1.0)
 
-rto.add_state('r', fix_initial=False, lower=0, ref=1000.0, defect_ref=1000.0,
-             rate_source='r_dot')
+rto.add_state('r', fix_initial=False, lower=0, ref=1000.0, defect_ref=1000.0)
 
-rto.add_state('v', fix_initial=False, lower=0.0001, ref=100.0, defect_ref=100.0,
-             rate_source='v_dot')
+rto.add_state('v', fix_initial=False, lower=0.0001, ref=100.0, defect_ref=100.0)
 
-rto.add_parameter('h', val=0.0, opt=False, units='m', dynamic=True)
-rto.add_parameter('T', val=0.0, opt=False, units='N', dynamic=False)
-rto.add_parameter('m', val=174200, opt=False, units='lbm')
-rto.add_parameter('mu_r', val=0.3, opt=False, units=None, dynamic=False)
+# rto.add_parameter('h', val=0.0, opt=False, units='m', dynamic=False)
+# rto.add_parameter('T', val=0.0, opt=False, units='N', dynamic=False)
+# rto.add_parameter('mu_r', val=0.3, opt=False, units=None, dynamic=False)
 rto.add_parameter('alpha', val=0.0, opt=False, units='deg')
 
 rto.add_timeseries_output('*')
@@ -132,43 +124,21 @@ rto.add_objective('r', loc='final', ref=1000.0) #  ref0=2000.0, ref=3000.0)
 
 rotate = dm.Phase(ode_class=GroundRollODEComp, transcription=dm.Radau(num_segments=3))
 
-#
-# Set the options on the optimization variables
-#
 rotate.set_time_options(fix_initial=False, duration_bounds=(1.0, 5), duration_ref=1.0)
+rotate.add_state('r', fix_initial=False, lower=0, ref=1000.0, defect_ref=1000.0)
+rotate.add_state('v', fix_initial=False, lower=0.0001, ref=100.0, defect_ref=100.0)
 
-rotate.add_state('r', fix_initial=False, lower=0, ref=1000.0, defect_ref=1000.0,
-             rate_source='r_dot')
-
-rotate.add_state('v', fix_initial=False, lower=0.0001, ref=100.0, defect_ref=100.0,
-             rate_source='v_dot')
-
-rotate.add_parameter('h', val=0.0, opt=False, units='m', dynamic=True)
-rotate.add_parameter('T', val=27000, opt=False, units='lbf', dynamic=False)
-rotate.add_parameter('m', val=174200, opt=False, units='lbm')
-rotate.add_parameter('mu_r', val=0.05, opt=False, units=None, dynamic=False)
+# rotate.add_parameter('h', val=0.0, opt=False, units='m', dynamic=False)
+# rotate.add_parameter('T', val=27000, opt=False, units='lbf', dynamic=False)
+# rotate.add_parameter('mu_r', val=0.05, opt=False, units=None, dynamic=False)
 
 rotate.add_polynomial_control('alpha', order=1, opt=True, units='deg', lower=0, upper=10, ref=10)
 
-# rotate.add_control('alpha', val=0.0, opt=True, lower=0, upper=10, units='deg')
-
 rotate.add_timeseries_output('*')
 
-# rotate.add_boundary_constraint('alpha', loc='final', equals=10, units='deg')
 
-# rotate.add_path_constraint('alpha_rate', lower=0, upper=3, units='deg/s')
+climb = dm.Phase(ode_class=TakeoffClimbODEComp, transcription=dm.Radau(num_segments=5))
 
-# rotate.add_objective('r', loc='final', ref=10000.0)
-
-# Fourth Phase - Rotate for single engine takeoff
-# liftoff until v2 (1.2 * v_stall) at 35 ft
-#
-
-climb = dm.Phase(ode_class=TakeoffClimbODEComp, transcription=dm.Radau(num_segments=10))
-
-#
-# Set the options on the optimization variables
-#
 climb.set_time_options(fix_initial=False, duration_bounds=(1, 100), duration_ref=1.0)
 
 climb.add_state('r', fix_initial=False, lower=0, ref=1000.0, defect_ref=1000.0)
@@ -176,18 +146,13 @@ climb.add_state('h', fix_initial=True, lower=0.0, ref=1.0, defect_ref=1.0)
 climb.add_state('v', fix_initial=False, lower=0.0001, ref=100.0, defect_ref=100.0)
 climb.add_state('gam', fix_initial=True, lower=0.0, ref=0.05, defect_ref=0.05)
 
-climb.add_parameter('T', val=27000, opt=False, units='lbf', dynamic=False)
-climb.add_parameter('m', val=174200, opt=False, units='lbm')
+# climb.add_parameter('T', val=27000, opt=False, units='lbf', dynamic=False)
 
 climb.add_control('alpha', opt=True, units='deg', lower=-10, upper=15, ref=10, rate_continuity=True, rate_continuity_scaler=1)
 
 climb.add_timeseries_output('*')
 
 
-
-
-
-# climb.add_objective('time', loc='final', ref=100.0)
 
 p.model.linear_solver = om.DirectSolver()
 # p.model.linear_solver = om.PETScKrylov()
@@ -208,6 +173,34 @@ traj.add_phase('v1_to_vr', v1_to_vr)
 traj.add_phase('rto', rto)
 traj.add_phase('rotate', rotate)
 traj.add_phase('climb', climb)
+
+
+traj.add_parameter('m', val=174200., opt=False, units='lbm',
+                   targets={'br_to_v1': ['m'], 'v1_to_vr': ['m'], 'rto': ['m'],
+                            'rotate': ['m'], 'climb': ['m']})
+
+traj.add_parameter('T_nominal', val=27000 * 2, opt=False, units='lbf', dynamic=False,
+                   targets={'br_to_v1': ['T']})
+
+traj.add_parameter('T_engine_out', val=27000, opt=False, units='lbf', dynamic=False,
+                   targets={'v1_to_vr': ['T'], 'rotate': ['T'], 'climb': ['T']})
+
+traj.add_parameter('T_shutdown', val=0.0, opt=False, units='lbf', dynamic=False,
+                   targets={'rto': ['T']})
+
+traj.add_parameter('mu_r_nominal', val=0.03, opt=False, units=None, dynamic=False,
+                   targets={'br_to_v1': ['mu_r'], 'v1_to_vr': ['mu_r'],  'rotate': ['mu_r']})
+
+traj.add_parameter('mu_r_braking', val=0.3, opt=False, units=None, dynamic=False,
+                   targets={'rto': ['mu_r']})
+
+# traj.add_parameter('h_runway', val=0., opt=False, units='ft',
+#                    targets={'br_to_v1': ['h'], 'v1_to_vr': ['h'], 'rto': ['h'],
+#                             'rotate': ['h']})
+
+traj.add_parameter('rho', val=1.225, opt=False, units='kg/m**3', dynamic=False,
+                   targets={'br_to_v1': ['rho'], 'v1_to_vr': ['rho'], 'rto': ['rho'],
+                            'rotate': ['rho']})
 
 #
 # Link the phases
@@ -247,7 +240,7 @@ p.set_val('traj.br_to_v1.states:r', br_to_v1.interpolate(ys=[0, 2500.0], nodes='
 p.set_val('traj.br_to_v1.states:v', br_to_v1.interpolate(ys=[0.0001, 100.0], nodes='state_input'))
 
 p.set_val('traj.br_to_v1.parameters:alpha', 0, units='deg')
-p.set_val('traj.br_to_v1.parameters:h', 0.0)
+# p.set_val('traj.br_to_v1.parameters:h', 0.0)
 
 #
 
@@ -259,7 +252,7 @@ p.set_val('traj.v1_to_vr.states:v', v1_to_vr.interpolate(ys=[100, 110.0], nodes=
 
 p.set_val('traj.v1_to_vr.parameters:alpha', 0.0, units='deg')
 
-p.set_val('traj.v1_to_vr.parameters:h', 0.0)
+# p.set_val('traj.v1_to_vr.parameters:h', 0.0)
 
 #
 
@@ -270,11 +263,8 @@ p.set_val('traj.rto.states:r', rto.interpolate(ys=[2500, 5000.0], nodes='state_i
 p.set_val('traj.rto.states:v', rto.interpolate(ys=[110, 0.0001], nodes='state_input'))
 
 p.set_val('traj.rto.parameters:alpha', 0.0, units='deg')
-p.set_val('traj.rto.parameters:h', 0.0)
-p.set_val('traj.rto.parameters:T', 0.0)
-p.set_val('traj.rto.parameters:mu_r', 0.3)
 
-#
+
 
 p.set_val('traj.rotate.t_initial', 35)
 p.set_val('traj.rotate.t_duration', 5)
@@ -284,7 +274,7 @@ p.set_val('traj.rotate.states:v', rotate.interpolate(ys=[80, 85.0], nodes='state
 
 p.set_val('traj.rotate.polynomial_controls:alpha', 0.0, units='deg')
 
-p.set_val('traj.rotate.parameters:h', 0.0)
+# p.set_val('traj.rotate.parameters:h', 0.0)
 
 #
 p.set_val('traj.climb.t_initial', 30)
@@ -296,71 +286,10 @@ p.set_val('traj.climb.states:h', climb.interpolate(ys=[0, 35.0], nodes='state_in
 p.set_val('traj.climb.states:gam', climb.interpolate(ys=[0, 5.0], nodes='state_input'), units='deg')
 
 p.set_val('traj.climb.controls:alpha', 5.0, units='deg')
-p.set_val('traj.climb.parameters:T', 27000.0, units='lbf')
-
-#
-# Solve for the optimal trajectory
-#
-# p.run_model()
-# p.check_partials(compact_print=True)
-#
-# p.run_model()
-#
-# from openmdao.utils.sc
-
-dm.run_problem(p, run_driver=True, simulate=False, make_plots=True) #, restart='dymos_simulation.db')
-om.n2(p)
-rotate.rhs_all.list_outputs(print_arrays=True, prom_name=True)
+# p.set_val('traj.climb.parameters:T', 27000.0, units='lbf')
 
 
-# rotate.rhs_all.list_outputs(print_arrays=True, prom_name=True)
-
-# import numpy as np
-# with np.printoptions(linewidth=1024):
-#     for var in ['T', 'calpha', 'D', 'F_r', 'mu_r', 'm']:
-#         try:
-#             print(var)
-#             print(p.get_val(f'traj.rotate.rhs_all.{var}'))
-#             print(p.get_val(f'traj.rotate.rhs_all.aero_force_comp.{var}'))
-#         except:
-#             pass
-
-# print(p.get_val('traj.rotate.rhs_all.aero_forces_comp.F_r'))
-# print(p.get_val('traj.rotate.rhs_all.F_r'))
-
-# p.driver.scaling_report()
-#
-# Get the explicitly simulated solution and plot the results
-#
-# exp_out = traj.simulate()
-
-#
-# fig, axes = plt.subplots(3, 1)
-#
-# for phase_name in ['br_to_v1', 'v1_to_vr', 'rotate', 'rto']: #, 'climb']:
-#
-#     axes[0].plot(p.get_val(f'traj.{phase_name}.timeseries.time'),
-#                  p.get_val(f'traj.{phase_name}.timeseries.states:r', units='ft'), 'o')
-#
-#     axes[0].plot(exp_out.get_val(f'traj.{phase_name}.timeseries.time'),
-#                  exp_out.get_val(f'traj.{phase_name}.timeseries.states:r', units='ft'), '-')
-#
-#     axes[1].plot(p.get_val(f'traj.{phase_name}.timeseries.time'),
-#                  p.get_val(f'traj.{phase_name}.timeseries.states:v', units='kn'), 'o')
-#
-#     axes[1].plot(exp_out.get_val(f'traj.{phase_name}.timeseries.time'),
-#                  exp_out.get_val(f'traj.{phase_name}.timeseries.states:v', units='kn'), '-')
-#
-#     try:
-#         axes[2].plot(p.get_val(f'traj.{phase_name}.timeseries.time'),
-#                      p.get_val(f'traj.{phase_name}.timeseries.F_r', units='N'), 'o')
-#
-#         axes[2].plot(exp_out.get_val(f'traj.{phase_name}.timeseries.time'),
-#                      exp_out.get_val(f'traj.{phase_name}.timeseries.F_r', units='N'), '-')
-#     except KeyError:
-#         pass
-#
-# plt.show()
+dm.run_problem(p, run_driver=True, simulate=True, make_plots=True) #, restart='dymos_simulation.db')
 
 
 if __name__ == '__main__':  # pragma: no cover
