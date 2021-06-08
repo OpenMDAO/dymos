39aef7c0789ec8f03ccfb3010954984dbd4fed9b
Kaushik Ponnapalli <kaushik.s.ponnapalli@nasa.gov>
2021-06-08 11:33:49 -0400

    removed old boundary_constraint_comp and accompanying tests

--- dymos/transcriptions/common/__init__.py
+++ dymos/transcriptions/common/__init__.py
@@ -1,4 +1,3 @@
-from .boundary_constraint_comp import BoundaryConstraintComp
 from .continuity_comp import RadauPSContinuityComp, GaussLobattoContinuityComp
 from .control_group import ControlGroup
 from .polynomial_control_group import PolynomialControlGroup
