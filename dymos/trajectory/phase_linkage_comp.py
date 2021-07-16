Fixed an issue with units in linkage constraints (#620)
* Fixed an issue where inputs being used in multiple connections assuming different units were causing problems with linkage constraints.

* cleanup

* progress, but potential slsqp issue

* down to the last failing test

* reworked linkage units test so that optimization is not required.

dymos/trajectory/phase_linkage_comp.py
@@ -3,6 +3,7 @@ import numpy as np
 import openmdao.api as om
 
 from ..options import options as dymos_options
+from ..utils.misc import _unspecified
 
 
 class PhaseLinkageComp(om.ExplicitComponent):
@@ -61,13 +62,26 @@ class PhaseLinkageComp(om.ExplicitComponent):
         phase_a = lnk['phase_a']
         phase_b = lnk['phase_b']
         shape = lnk['shape']
-        units = lnk['units']
 
         var_a = lnk['constraint_name'] if lnk['constraint_name'] else lnk['var_a'].split('.')[-1]
         var_b = lnk['constraint_name'] if lnk['constraint_name'] else lnk['var_b'].split('.')[-1]
 
         loc_a = lnk['loc_a']
         loc_b = lnk['loc_b']
+
+        units_a = lnk['units_a'] if lnk['units_a'] is not _unspecified else None
+        units_b = lnk['units_b'] if lnk['units_b'] is not _unspecified else None
+        units = lnk['units'] if lnk['units'] is not _unspecified else None
+
+        if units_a is None or units is None:
+            lnk._conv_a, lnk._offset_a = 1.0, 0.0
+        else:
+            lnk._conv_a, lnk._offset_a = om.unit_conversion(units_a, units)
+
+        if units_b is None or units is None:
+            lnk._conv_b, lnk._offset_b = 1.0, 0.0
+        else:
+            lnk._conv_b, lnk._offset_b = om.unit_conversion(units_b, units)
 
         input_a = f'{phase_a}:{var_a}'
         input_b = f'{phase_b}:{var_b}'
@@ -81,12 +95,12 @@ class PhaseLinkageComp(om.ExplicitComponent):
         lnk._output = output
 
         try:
-            self.add_input(name=input_a, shape=ishape, val=np.zeros(ishape), units=units)
+            self.add_input(name=input_a, shape=ishape, val=np.zeros(ishape), units=units_a)
         except ValueError as e:
             pass
 
         try:
-            self.add_input(name=input_b, shape=ishape, val=np.zeros(ishape), units=units)
+            self.add_input(name=input_b, shape=ishape, val=np.zeros(ishape), units=units_b)
         except ValueError as e:
             pass
 
@@ -104,8 +118,11 @@ class PhaseLinkageComp(om.ExplicitComponent):
         cs_a = rs if loc_a == 'initial' else size + rs
         cs_b = rs if loc_b == 'initial' else size + rs
 
-        self.declare_partials(of=output, wrt=input_a, rows=rs, cols=cs_a, val=lnk['sign_a'])
-        self.declare_partials(of=output, wrt=input_b, rows=rs, cols=cs_b, val=lnk['sign_b'])
+        self.declare_partials(of=output, wrt=input_a, rows=rs, cols=cs_a,
+                              val=lnk['sign_a'] * lnk._conv_a)
+
+        self.declare_partials(of=output, wrt=input_b, rows=rs, cols=cs_b,
+                              val=lnk['sign_b'] * lnk._conv_b)
 
     def compute(self, inputs, outputs):
         """
@@ -125,4 +142,7 @@ class PhaseLinkageComp(om.ExplicitComponent):
             idxs_b = lnk._idxs_b
             output = lnk._output
 
-            outputs[output] = lnk['sign_a'] * inputs[input_a][idxs_a] + lnk['sign_b'] * inputs[input_b][idxs_b]
+            a_val = lnk['sign_a'] * (inputs[input_a][idxs_a] + lnk._offset_a) * lnk._conv_a
+            b_val = lnk['sign_b'] * (inputs[input_b][idxs_b] + lnk._offset_b) * lnk._conv_b
+
+            outputs[output] = a_val + b_val