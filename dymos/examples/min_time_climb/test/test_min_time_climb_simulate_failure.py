0d747168da0588f702f1d58363658f700edb3247
Rob Falck <rfalck@nasa.gov>
2021-08-04 09:04:22 -0400

    interpolation changes changed the failure mode of the segment simulation error test.

--- dymos/examples/min_time_climb/test/test_min_time_climb_simulate_failure.py
+++ dymos/examples/min_time_climb/test/test_min_time_climb_simulate_failure.py
@@ -93,7 +93,7 @@ class TestMinTimeClimbSimulateFailure(unittest.TestCase):
 
         with self.assertRaises(om.AnalysisError) as e:
             dm.run_problem(p, run_driver=False, simulate=True)
-        expected = '\'traj.phases.phase0.segments.segment_2\' <class SegmentSimulationComp>: ' \
+        expected = '\'traj.phases.phase0.segments.segment_1\' <class SegmentSimulationComp>: ' \
                    'Error calling compute(), solve_ivp failed: Required step size is less than ' \
                    'spacing between numbers. Dynamics changing too dramatically'
         self.assertEqual(str(e.exception), expected)
