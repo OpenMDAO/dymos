f34f8e906f938fac6418f61bd87c86c09eaaab71
Rob Falck <rfalck@nasa.gov>
2021-09-01 15:05:20 -0400

    progress

--- dymos/transcriptions/timestepping/test/test_explicit_shooting.py
+++ dymos/transcriptions/timestepping/test/test_explicit_shooting.py
@@ -62,9 +62,67 @@ class TestExplicitShooting(unittest.TestCase):
         assert_near_equal(t_f, 1.8016, tolerance=5.0E-3)
 
         with np.printoptions(linewidth=1024):
-            cpd = prob.check_partials(compact_print=True, method='cs')
-            assert_check_partials(cpd)
+            cpd = prob.check_partials(compact_print=True, method='fd')
+            assert_check_partials(cpd, atol=1.0E-5, rtol=1.0E-5)
 
         prob.model.list_outputs(print_arrays=True)
 
         prob.list_problem_vars()
+
+
+if __name__ == '__main__':
+    prob = om.Problem()
+
+    prob.driver = om.pyOptSparseDriver(optimizer='SNOPT')
+    prob.driver.opt_settings['Verify level'] = 3
+    prob.driver.opt_settings['iSumm'] = 6
+
+    tx = dm.transcriptions.ExplicitShooting(num_segments=5, grid='gauss-lobatto',
+                                            order=3, num_steps_per_segment=20, compressed=True)
+
+    phase = dm.Phase(ode_class=BrachistochroneODE, transcription=tx)
+
+    phase.set_time_options(units='s', fix_initial=True, duration_bounds=(1.0, 10.0))
+
+    # automatically discover states
+    phase.set_state_options('x', fix_initial=True)
+    phase.set_state_options('y', fix_initial=True)
+    phase.set_state_options('v', fix_initial=True)
+
+    phase.add_parameter('g', val=9.80665, units='m/s**2', opt=False)
+    phase.add_control('theta', val=45.0, units='deg', opt=True, lower=1.0E-6, upper=179.9)
+
+    phase.add_boundary_constraint('x', loc='final', equals=10.0)
+    phase.add_boundary_constraint('y', loc='final', equals=5.0)
+
+    prob.model.add_subsystem('phase0', phase)
+
+    phase.add_objective('time', loc='final')
+
+    prob.setup(force_alloc_complex=True)
+
+    prob.set_val('phase0.t_initial', 0.0)
+    prob.set_val('phase0.t_duration', 2)
+    prob.set_val('phase0.states:x', 0.0)
+    prob.set_val('phase0.states:y', 10.0)
+    prob.set_val('phase0.states:v', 1.0E-6)
+    prob.set_val('phase0.parameters:g', 9.80665, units='m/s**2')
+    prob.set_val('phase0.controls:theta', phase.interp('theta', ys=[0.01, 90]), units='deg')
+
+    prob.run_driver()
+
+    # x_f = prob.get_val('phase0.integrator.states_out:x')
+    # y_f = prob.get_val('phase0.integrator.states_out:y')
+    # t_f = prob.get_val('phase0.integrator.t_final')
+    #
+    # assert_near_equal(x_f, 10.0, tolerance=1.0E-5)
+    # assert_near_equal(y_f, 5.0, tolerance=1.0E-5)
+    # assert_near_equal(t_f, 1.8016, tolerance=5.0E-3)
+    #
+    # with np.printoptions(linewidth=1024):
+    #     cpd = prob.check_partials(compact_print=True, method='cs')
+    #     assert_check_partials(cpd)
+    #
+    # prob.model.list_outputs(print_arrays=True)
+    #
+    # prob.list_problem_vars()
