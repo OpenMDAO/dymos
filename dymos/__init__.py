6c72d4dc74a6927095ed71148d3e40d1771c464a
Eliot Aretskin-Hariton <eliot.d.aretskin-hariton@nasa.gov>
2021-11-04 18:04:23 -0400

    added ExplicitShooting to transcription import so you dont have to call dm.transcription.ExplicitShooting

--- dymos/__init__.py
+++ dymos/__init__.py
@@ -1,7 +1,7 @@
 __version__ = '1.2.1-dev'
 
 from .phase import Phase
-from .transcriptions import GaussLobatto, Radau
+from .transcriptions import GaussLobatto, Radau, ExplicitShooting
 from .trajectory.trajectory import Trajectory
 from .run_problem import run_problem
 from .load_case import load_case
