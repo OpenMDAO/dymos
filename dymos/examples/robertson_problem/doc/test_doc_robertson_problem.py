25b4d2eae431e52cd82c0de98574ba3d80699a82
Joel Martin <39242266+joel-martin@users.noreply.github.com>
2021-11-15 11:01:39 +0100

    Move problem definition into class

--- dymos/examples/robertson_problem/doc/test_doc_robertson_problem.py
+++ dymos/examples/robertson_problem/doc/test_doc_robertson_problem.py
@@ -12,12 +12,57 @@ from dymos.examples.robertson_problem.robertson_ode import RobertsonODE
 @use_tempdirs
 class TestRobertsonProblemForDocs(unittest.TestCase):
 
+    def robertson_problem(self, t_final=1.0):
+
+        import openmdao.api as om
+        import dymos as dm
+
+        #
+        # Initialize the Problem
+        #
+        p = om.Problem(model=om.Group())
+
+        #
+        # Create a trajectory and add a phase to it
+        #
+        traj = p.model.add_subsystem('traj', dm.Trajectory())
+
+        phase = traj.add_phase('phase0',
+                               dm.Phase(ode_class=RobertsonODE,
+                                        transcription=dm.GaussLobatto(num_segments=50)
+                                        ))
+
+        #
+        # Set the variables
+        #
+        phase.set_time_options(fix_initial=True, fix_duration=True)
+
+        phase.add_state('x0', fix_initial=True, fix_final=False, rate_source='xdot', targets='x')
+        phase.add_state('y0', fix_initial=True, fix_final=False, rate_source='ydot', targets='y')
+        phase.add_state('z0', fix_initial=True, fix_final=False, rate_source='zdot', targets='z')
+
+        #
+        # Setup the Problem
+        #
+        p.setup(check=True)
+
+        #
+        # Set the initial values
+        #
+        p['traj.phase0.t_initial'] = 0.0
+        p['traj.phase0.t_duration'] = t_final
+
+        p.set_val('traj.phase0.states:x0', phase.interp('x0', ys=[1.0, 0.7]))
+        p.set_val('traj.phase0.states:y0', phase.interp('y0', ys=[0.0, 1e-5]))
+        p.set_val('traj.phase0.states:z0', phase.interp('z0', ys=[0.0, 0.3]))
+
+        return p
+
     def test_robertson_problem_for_docs(self):
 
         import openmdao.api as om
         from dymos.utils.testing_utils import assert_check_partials
         from openmdao.utils.assert_utils import assert_near_equal
-        import dymos as dm
         import matplotlib.pyplot as plt
 
         num_nodes = 3
@@ -41,50 +86,8 @@ class TestRobertsonProblemForDocs(unittest.TestCase):
         assert_near_equal(p.get_val('ydot'), [-3.00099996E7, -3.01996E5, -3.26E3])
         assert_near_equal(p.get_val('zdot'), [3.0E7, 3.0E5, 3.0E3])
 
-        def robertson_problem(t_final=1.):
-            #
-            # Initialize the Problem
-            #
-            p = om.Problem(model=om.Group())
-
-            #
-            # Create a trajectory and add a phase to it
-            #
-            traj = p.model.add_subsystem('traj', dm.Trajectory())
-
-            phase = traj.add_phase('phase0',
-                                   dm.Phase(ode_class=RobertsonODE,
-                                            transcription=dm.GaussLobatto(num_segments=50)
-                                           ))
-
-            #
-            # Set the variables
-            #
-            phase.set_time_options(fix_initial=True, fix_duration=True)
-
-            phase.add_state('x0', fix_initial=True, fix_final=False, rate_source='xdot', targets='x')
-            phase.add_state('y0', fix_initial=True, fix_final=False, rate_source='ydot', targets='y')
-            phase.add_state('z0', fix_initial=True, fix_final=False, rate_source='zdot', targets='z')
-
-            #
-            # Setup the Problem
-            #
-            p.setup(check=True)
-
-            #
-            # Set the initial values
-            #
-            p['traj.phase0.t_initial'] = 0.0
-            p['traj.phase0.t_duration'] = t_final
-
-            p.set_val('traj.phase0.states:x0', phase.interp('x0', ys=[1.0, 0.7]))
-            p.set_val('traj.phase0.states:y0', phase.interp('y0', ys=[0.0, 1e-5]))
-            p.set_val('traj.phase0.states:z0', phase.interp('z0', ys=[0.0, 0.3]))
-
-            return p
-
         # just set up the problem, test it elsewhere
-        p = robertson_problem(t_final = 40)
+        p = self.robertson_problem(t_final = 40)
 
         p.run_model()
 
