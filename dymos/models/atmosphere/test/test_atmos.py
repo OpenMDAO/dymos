4e89d6f1a688d661a6cb50619f34c0c966802df9
Rob Falck <rfalck@nasa.gov>
2022-04-07 15:45:43 -0400

    USatm1976Comp now accepts altitude as either geopotential or geodetic (#735)
    * USatm1976Comp now accepts altitude as either geopotential (the default and previous implementation) or as geodetic.
    
    * used some timings to confirm that an if statement is still faster than the lambda way of doing this.

--- dymos/models/atmosphere/test/test_atmos.py
+++ dymos/models/atmosphere/test/test_atmos.py
@@ -9,7 +9,7 @@ from dymos.models.atmosphere.atmos_1976 import USatm1976Comp, USatm1976Data
 
 class TestAtmosphere(unittest.TestCase):
 
-    def test_temperature_comp(self):
+    def test_atmos_comp_geopotential(self):
         n = USatm1976Data.alt.size
 
         p = om.Problem(model=om.Group())
@@ -28,12 +28,45 @@ class TestAtmosphere(unittest.TestCase):
         rho = p.get_val('atmos.rho', units='slug/ft**3')
         sos = p.get_val('atmos.sos', units='ft/s')
 
-        assert_near_equal(T, USatm1976Data.T, tolerance=1.0E-2)
-        assert_near_equal(P, USatm1976Data.P, tolerance=1.0E-2)
-        assert_near_equal(rho, USatm1976Data.rho, tolerance=1.0E-2)
-        assert_near_equal(sos, USatm1976Data.a, tolerance=1.0E-2)
+        assert_near_equal(T, USatm1976Data.T, tolerance=1.0E-4)
+        assert_near_equal(P, USatm1976Data.P, tolerance=1.0E-4)
+        assert_near_equal(rho, USatm1976Data.rho, tolerance=1.0E-4)
+        assert_near_equal(sos, USatm1976Data.a, tolerance=1.0E-4)
 
         cpd = p.check_partials(method='cs', out_stream=None)
+        assert_check_partials(cpd)
+
+    def test_atmos_comp_geodetic(self):
+        n = USatm1976Data.alt.size
+
+        p = om.Problem(model=om.Group())
+
+        ivc = p.model.add_subsystem('ivc', subsys=om.IndepVarComp(), promotes_outputs=['*'])
+        ivc.add_output(name='alt', val=USatm1976Data.alt, units='ft')
+
+        p.model.add_subsystem('atmos', subsys=USatm1976Comp(num_nodes=n, h_def='geodetic'))
+        p.model.connect('alt', 'atmos.h')
+
+        p.setup(force_alloc_complex=True)
+
+        h = USatm1976Data.alt * 0.3048  # altitude data in meters
+        R0 = 6_356_766  # US 1976 std atm R0 in m
+        p.set_val('alt', R0 / (R0 - h) * h, units='m')  # US 1976 std atm geopotential altitude to geodetic (m)
+
+        p.run_model()
+
+        T = p.get_val('atmos.temp', units='degR')
+        P = p.get_val('atmos.pres', units='psi')
+        rho = p.get_val('atmos.rho', units='slug/ft**3')
+        sos = p.get_val('atmos.sos', units='ft/s')
+
+        assert_near_equal(T, USatm1976Data.T, tolerance=1.0E-4)
+        assert_near_equal(P, USatm1976Data.P, tolerance=1.0E-4)
+        assert_near_equal(rho, USatm1976Data.rho, tolerance=1.0E-4)
+        assert_near_equal(sos, USatm1976Data.a, tolerance=1.0E-4)
+
+        with np.printoptions(linewidth=100000):
+            cpd = p.check_partials(method='cs')
         assert_check_partials(cpd)
 
 
