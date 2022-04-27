5fa5ab686339275c24c669ce48284b5c8e51a3d1
Rob Falck <rfalck@nasa.gov>
2022-04-27 10:50:18 -0400

    added test for error message when more than one boundary/path constraint involves the same parameter

--- dymos/examples/brachistochrone/test/test_duplicate_constraints.py
+++ dymos/examples/brachistochrone/test/test_duplicate_constraints.py
@@ -396,7 +396,6 @@ class TestDuplicateConstraints(unittest.TestCase):
 
         self.assertEqual(str(e.exception), expected)
 
-
     def test_duplicate_path_constraint_equivalent_negative_indices(self):
         p = om.Problem(model=om.Group())
 
@@ -434,5 +433,46 @@ class TestDuplicateConstraints(unittest.TestCase):
 
         expected = 'Duplicate constraint in phase traj0.phases.phase0. The following indices of `theta` are ' \
                    'used in multiple path constraints:\n{0}'
+
+        self.assertEqual(str(e.exception), expected)
+
+    def test_duplicate_parameter_constraint(self):
+        p = om.Problem(model=om.Group())
+
+        p.driver = om.ScipyOptimizeDriver()
+        p.driver.declare_coloring(tol=1.0E-12)
+
+        tx = dm.Radau(num_segments=10, order=3)
+
+        traj = dm.Trajectory()
+        phase = dm.Phase(ode_class=BrachistochroneODE, transcription=tx)
+        p.model.add_subsystem('traj0', traj)
+        traj.add_phase('phase0', phase)
+
+        phase.set_time_options(fix_initial=True, duration_bounds=(.5, 10))
+
+        phase.add_state('x', fix_initial=False, fix_final=False)
+        phase.add_state('y', fix_initial=False, fix_final=False)
+
+        # Note that by omitting the targets here Dymos will automatically attempt to connect
+        # to a top-level input named 'v' in the ODE, and connect to nothing if it's not found.
+        phase.add_state('v', fix_initial=False, fix_final=False)
+
+        phase.add_control('theta',
+                          continuity=True, rate_continuity=True,
+                          units='deg', lower=0.01, upper=179.9)
+
+        phase.add_parameter('g', targets=['g'], units='m/s**2', opt=True)
+
+        # Now mistakenly add a boundary constraint at the same loc, variable, and different but equivalent_negative indices
+        phase.add_boundary_constraint('g', loc='initial', upper=9.80665)
+        phase.add_path_constraint('g', lower=0)
+
+        with self.assertRaises(RuntimeError) as e:
+            p.setup()
+
+        expected = "In phase traj0.phases.phase0, parameter g is subject to multiple boundary or path constraints.\n" \
+                   "Parameters are single values that do not change in time, and may only be used in a single " \
+                   "boundary or path constraint."
 
         self.assertEqual(str(e.exception), expected)
