7505db0c9e3f9edc257f4be8d66942edc0a195e4
Rob Falck <rfalck@nasa.gov>
2022-05-04 14:32:31 -0400

    Version 1.5.0 (#745)
    * Release notes for 1.5.0.  Fixed the github workflow_dispatch issue on this branch as well.
    
    * Bump version: 1.4.1-dev → 1.5.0-dev
    
    * Bump version: 1.5.0-dev → 1.5.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ optional_dependencies['all'] = sorted([
 
 
 setup(name='dymos',
-    version='1.4.1-dev',
+    version='1.5.0',
     description='Open-Source Optimization of Dynamic Multidisciplinary Systems',
     long_description='''
 Dymos is a framework for the simulation and optimization of dynamical systems within the OpenMDAO Multidisciplinary Analysis and Optimization environment.
