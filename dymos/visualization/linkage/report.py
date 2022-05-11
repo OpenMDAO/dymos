eaf75c707b9eab268241b3c80f61c15fe32c1850
Thaddeus Kollar <tad.kollar@nasa.gov>
2022-05-11 15:34:47 -0400

    Added docstrings

--- dymos/visualization/linkage/report.py
+++ dymos/visualization/linkage/report.py
@@ -7,9 +7,25 @@ import os
 
 CBN = 'children_by_name'
 
-def create_linkage_report(traj, output_file='linkage_report.html',
+def create_linkage_report(traj, output_file: str='linkage_report.html',
                           show_all_vars=False,
                           title='Dymos Linkage Report', embedded=False):
+    """
+    Create a tree based on the trajectory and linkages, then export as HTML.
+
+    Parameters
+    ----------
+    traj : Trajectory
+        The Dymos Trajectory containing the phase and linkage data.
+    output_file : str
+        Where to write the HTML file with the report diagram.
+    show_all_vars : bool
+        If True, display all parameters and controls even if not linked.
+    title : str
+        The title to display in the HTML document.
+    embedded : bool
+        Whether the file is to be included in another. If True, leave out some tags.
+    """
     model_data = _trajectory_to_dict(traj)
     model_data['connections_list'] = _linkages_to_list(traj, model_data)
     _convert_dicts_to_lists(model_data['tree'], show_all_vars)
@@ -31,10 +47,11 @@ def create_linkage_report(traj, output_file='linkage_report.html',
     # print(pretty)
 
     HtmlPreprocessor(os.path.join(reports_dir, "report_template.html"), output_file,
-                    start_path=vis_dir, allow_overwrite=True, var_dict=html_vars,
+                    search_path=[vis_dir, reports_dir], allow_overwrite=True, var_dict=html_vars,
                     verbose=False).run()
 
 def _tree_var(var_name, var_class):
+    """ Create a dict to represent a variable in the tree. """
     return {
         'name': var_name,
         # 'type': 'variable',
@@ -43,6 +60,19 @@ def _tree_var(var_name, var_class):
     }
 
 def _trajectory_to_dict(traj):
+    """
+    Iterate over the variables in the trajectory's phases and create a hierarchical structure.
+
+    Parameters
+    ----------
+    traj : Trajectory
+        The trajectory containing the phases to find variables in.
+    
+    Returns
+    -------
+    dict
+        A dictionary in the form: root -> phases -> conditions (initial/final) -> variables.
+    """
     model_data = {
         'tree': {
             'name': 'root',
@@ -99,7 +129,26 @@ def _trajectory_to_dict(traj):
 
     return model_data
 
-def _is_fixed(var_name, class_name, phase, loc):
+def _is_fixed(var_name: str, class_name: str, phase: Phase, loc: str):
+    """
+    Determine whether a variable is fixed or not.
+
+    Parameters
+    ----------
+    var_name : str
+        Identifier of the variable as known to the phase.
+    class_name : str
+        The type of variable.
+    phase : Phase
+        The phase where the variable is found.
+    loc : str
+        Either 'initial' or 'final'.
+
+    Returns
+    -------
+    bool
+        True if the variable is fixed, otherwise False.
+    """
     fixed = False
 
     if class_name == 'time':
@@ -116,6 +165,23 @@ def _is_fixed(var_name, class_name, phase, loc):
     return bool(fixed)
 
 def _linkages_to_list(traj, model_data):
+    """
+    Extract variables from the trajectory's linkages dict and put them in an array.
+
+    Also flag in the model_data dict whether a variable is linked and/or fixed.
+
+    Parameters
+    ----------
+    traj : Trajectory
+        The trajectory with the linkages structure.
+    model_data : dict
+        Data extracted from the trajectory's phases.
+
+    Returns
+    -------
+    list
+        Dict objects with src, tgt, src_fixed, and tgt_fixed keys.
+    """
     linkages = []
     tree = model_data['tree']
 
@@ -152,6 +218,7 @@ def _linkages_to_list(traj, model_data):
     return linkages
 
 def _display_child(child, show_all_vars):
+    """ Determine whether the object should be included in the diagram. """
     if show_all_vars is True:
         return True
 
@@ -164,6 +231,7 @@ def _display_child(child, show_all_vars):
     return False
 
 def _convert_dicts_to_lists(tree_dict, show_all_vars):
+    """ Convert all children_by_name dicts to lists. """
     if CBN in tree_dict:
         tree_dict['children'] = []
         for child_name, child in tree_dict[CBN].items():
