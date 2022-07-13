64b044a4f1da75de363aed68d6581d74d369f104
Rob Falck <rfalck@nasa.gov>
2022-07-13 16:16:55 -0400

    Trajectory.phases subgroup is now available before setup. (#782)
    Added finite burn orbit raise test with connected phases to test access to the phases subgroup before setup.

--- dymos/examples/finite_burn_orbit_raise/test/test_ex_two_burn_orbit_raise_mpi.py
+++ dymos/examples/finite_burn_orbit_raise/test/test_ex_two_burn_orbit_raise_mpi.py
@@ -17,7 +17,7 @@ from dymos.examples.finite_burn_orbit_raise.finite_burn_orbit_raise_problem impo
 class TestExampleTwoBurnOrbitRaiseMPI(unittest.TestCase):
     N_PROCS = 3
 
-    def test_ex_two_burn_orbit_raise(self):
+    def test_ex_two_burn_orbit_raise_mpi(self):
         optimizer = 'IPOPT'
 
         p = two_burn_orbit_raise_problem(transcription='gauss-lobatto', transcription_order=3,
@@ -26,6 +26,17 @@ class TestExampleTwoBurnOrbitRaiseMPI(unittest.TestCase):
 
         if p.model.traj.phases.burn2 in p.model.traj.phases._subsystems_myproc:
             assert_near_equal(p.get_val('traj.burn2.states:deltav')[-1], 0.3995,
+                              tolerance=2.0E-3)
+
+    def test_ex_two_burn_orbit_raise_connected_mpi(self):
+        optimizer = 'IPOPT'
+
+        p = two_burn_orbit_raise_problem(transcription='gauss-lobatto', transcription_order=3,
+                                         compressed=False, optimizer=optimizer, simulate=False,
+                                         connected=True, show_output=False)
+
+        if p.model.traj.phases.burn2 in p.model.traj.phases._subsystems_myproc:
+            assert_near_equal(p.get_val('traj.burn2.states:deltav')[0], 0.3995,
                               tolerance=2.0E-3)
 
 
