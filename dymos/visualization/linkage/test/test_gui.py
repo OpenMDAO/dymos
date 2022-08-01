7953f867e02fd05775d7773c0760a296d16f6df2
Tad Kollar <tad.kollar@nasa.gov>
2022-08-01 16:51:57 -0400

    Fixed two problems with the linkage report test (#793)
    * Added playwright and aiounittest to setup.py
    
    * Used linkage_report
    _ui_test.py instead of test_linkage_report_ui.py
    
    * removed test_linkage_report_ui.py

--- dymos/visualization/linkage/test/test_gui.py
+++ dymos/visualization/linkage/test/test_gui.py
@@ -3,7 +3,7 @@ import unittest
 import os
 
 os.system("playwright install")
-from test_linkage_report_ui import dymos_linkage_gui_test_case
+from linkage_report_ui_test import dymos_linkage_gui_test_case
 
 if __name__ == "__main__":
     unittest.main()
