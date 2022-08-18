7110c5c136034b63caee687b188727b7538a8bd3
kanekosh <shugok@umich.edu>
2022-08-18 16:35:54 -0400

    modified docs and added tests

---
+++ dymos/examples/cart_pole/cartpole_dynamics.py
@@ -0,0 +1,123 @@
+"""
+Cart-pole dynamics (ODE)
+"""
+
+import numpy as np
+import openmdao.api as om
+
+class CartPoleDynamics(om.ExplicitComponent):
+    """
+    Computes the time derivatives of states given state variables and control inputs.
+
+    Parameters
+    ----------
+    m_cart : float
+        Mass of cart.
+    m_pole : float
+        Mass of pole.
+    l_pole : float
+        Length of pole.
+    theta : 1d array
+        Angle of pole, 0 for vertical downward, positive counter clockwise.
+    theta_dot : 1d array
+        Angluar velocity of pole.
+    f : 1d array
+        x-wise force applied to the cart.
+
+    Returns
+    -------
+    x_dotdot : 1d array
+        Acceleration of cart in x direction.
+    theta_dotdot : 1d array
+        Angular acceleration of pole.
+    e_dot : 1d array
+        Rate of "energy" state.
+    """
+
+    def initialize(self):
+        self.options.declare('num_nodes', default=1, desc='number of nodes to be evaluated (i.e., length of vectors x, theta, etc)')
+        self.options.declare('g', default=9.81, desc='gravity constant')
+
+    def setup(self):
+        nn = self.options['num_nodes']
+
+        # --- inputs ---
+        # cart-pole parameters
+        self.add_input('m_cart', shape=(1,), units='kg', desc='cart mass')
+        self.add_input('m_pole', shape=(1,), units='kg', desc='pole mass')
+        self.add_input('l_pole', shape=(1,), units='m', desc='pole length')
+        # state variables. States x, x_dot, energy have no influence on the outputs, so we don't need them as inputs.
+        self.add_input('theta', shape=(nn,), units='rad', desc='pole angle')
+        self.add_input('theta_dot', shape=(nn,), units='rad/s', desc='pole angle velocity')
+        # control input
+        self.add_input('f', shape=(nn,), units='N', desc='force applied to cart in x direction')
+
+        # --- outputs ---
+        # rate of states (accelerations)
+        self.add_output('x_dotdot', shape=(nn,), units='m/s**2', desc='x acceleration of cart')
+        self.add_output('theta_dotdot', shape=(nn,), units='rad/s**2', desc='angular acceleration of pole')
+        # also computes force**2, which will be integrated to compute the objective
+        self.add_output('e_dot', shape=(nn,), units='N**2', desc='square of force to be integrated')
+
+        # ---  partials ---.
+        # Jacobian of outputs w.r.t. state/control inputs is diagonal because each node (corresponds to time discretization) is independent
+        self.declare_partials(of=['*'], wrt=['theta', 'theta_dot', 'f'], method='exact', rows=np.arange(nn), cols=np.arange(nn))
+
+        # partials of outputs w.r.t. cart-pole parameters. I'll use complex-step for this, but still declare the sparsity structure.
+        # NOTE: since the cart-pole parameters are fixed during optimization, these partials are not necessary to declare.
+        self.declare_partials(of=['*'], wrt=['m_cart', 'm_pole', 'l_pole'], method='cs', rows=np.arange(nn), cols=np.zeros(nn))
+        self.declare_coloring(wrt=['m_cart', 'm_pole', 'l_pole'], method='cs', show_summary=False)
+        self.set_check_partial_options(wrt=['m_cart', 'm_pole', 'l_pole'], method='fd', step=1e-7)
+
+    def compute(self, inputs, outputs):
+        g = self.options['g']
+        mc = inputs['m_cart']
+        mp = inputs['m_pole']
+        lpole = inputs['l_pole']
+        theta = inputs['theta']
+        omega = inputs['theta_dot']
+        f = inputs['f']
+
+        sint = np.sin(theta)
+        cost = np.cos(theta)
+        det = mp * lpole * cost**2 - lpole * (mc + mp)
+        outputs['x_dotdot'] = (-mp * lpole * g * sint * cost - lpole * (f + mp * lpole * omega**2 * sint)) / det
+        outputs['theta_dotdot'] = ((mc + mp) * g * sint + cost * (f + mp * lpole * omega**2 * sint)) / det
+
+        outputs['e_dot'] = f**2
+
+    def compute_partials(self, inputs, jacobian):
+        g = self.options['g']
+        mc = inputs['m_cart']
+        mp = inputs['m_pole']
+        lpole = inputs['l_pole']
+        theta = inputs['theta']
+        theta_dot = inputs['theta_dot']
+        f = inputs['f']
+
+        # --- derivatives of x_dotdot ---
+        # Collecting Theta Derivative
+        low = mp * lpole * np.cos(theta)**2 - lpole * mc - lpole * mp
+        dhigh = mp * g * lpole * np.sin(theta)**2 - mp * g * lpole * np.cos(theta)**2 - lpole**2 * mp * theta_dot**2 * np.cos(theta)
+        high = -mp * g * lpole * np.cos(theta) * np.sin(theta) - lpole * f - lpole**2 * mp * theta_dot**2 * np.sin(theta)
+        dlow = 2. * mp * lpole * np.cos(theta) * (-np.sin(theta))
+
+        jacobian['x_dotdot', 'theta'] = (low * dhigh - high * dlow) / low**2
+        jacobian['x_dotdot', 'theta_dot'] = -2. * theta_dot * lpole**2 * mp * np.sin(theta) / (mp * lpole * np.cos(theta)**2 - lpole * mc - lpole * mp)
+        jacobian['x_dotdot', 'f'] = -lpole / (mp * lpole * np.cos(theta)**2 - lpole * mc - lpole * mp)
+
+        # --- derivatives of theta_dotdot ---
+        # Collecting Theta Derivative
+        low = mp * lpole * np.cos(theta)**2 - lpole * mc - lpole * mp
+        dlow = 2. * mp * lpole * np.cos(theta) * (-np.sin(theta))
+        high = (mc + mp) * g * np.sin(theta) + f * np.cos(theta) + mp * lpole * theta_dot**2 * np.sin(theta) * np.cos(theta)
+        dhigh = (mc + mp) * g * np.cos(theta) - f * np.sin(theta) + mp * lpole * theta_dot**2 * (np.cos(theta)**2 - np.sin(theta)**2)
+        
+        jacobian['theta_dotdot', 'theta'] = (low * dhigh - high * dlow) / low**2
+        jacobian['theta_dotdot', 'theta_dot'] = 2. * theta_dot * mp * lpole * np.sin(theta) * np.cos(theta) / (mp * lpole * np.cos(theta)**2 - lpole * mc - lpole * mp)
+        jacobian['theta_dotdot', 'f'] = np.cos(theta) / (mp * lpole * np.cos(theta)**2 - lpole * mc - lpole * mp)
+
+        # --- derivatives of e_dot ---
+        jacobian['e_dot', 'theta'] = 0.
+        jacobian['e_dot', 'theta_dot'] = 0.
+        jacobian['e_dot', 'f'] = 2. * f
---
+++ dymos/examples/cart_pole/test/test_cartpole_opt.py
@@ -0,0 +1,99 @@
+import unittest
+
+import numpy as np
+
+import openmdao.api as om
+from openmdao.utils.assert_utils import assert_near_equal
+from openmdao.utils.testing_utils import use_tempdirs, require_pyoptsparse
+
+import dymos as dm
+from dymos.examples.cart_pole.cartpole_dynamics import CartPoleDynamics
+
+
+@use_tempdirs
+class TestCartPoleOptimization(unittest.TestCase):
+
+    @require_pyoptsparse(optimizer='SNOPT')
+    def test_optimization(self):
+
+        p = om.Problem()
+
+        # --- instantiate trajectory and phase, setup transcription ---
+        traj = dm.Trajectory()
+        p.model.add_subsystem('traj', traj)
+        phase = dm.Phase(transcription=dm.GaussLobatto(num_segments=40, order=3, compressed=True, solve_segments=False), ode_class=CartPoleDynamics)
+        # NOTE: set solve_segments=True to do solver-based shooting
+        traj.add_phase('phase', phase)
+
+        # --- set state and control variables ---
+        phase.set_time_options(fix_initial=True, fix_duration=True, duration_val=2., units='s')
+        # declare state variables. You can also set lower/upper bounds and scalings here.
+        phase.add_state('x', fix_initial=True, lower=-2, upper=2, rate_source='x_dot', shape=(1,), ref=1, defect_ref=1, units='m')
+        phase.add_state('x_dot', fix_initial=True, rate_source='x_dotdot', shape=(1,), ref=1, defect_ref=1, units='m/s')
+        phase.add_state('theta', fix_initial=True, rate_source='theta_dot', shape=(1,), ref=1, defect_ref=1, units='rad')
+        phase.add_state('theta_dot', fix_initial=True, rate_source='theta_dotdot', shape=(1,), ref=1, defect_ref=1, units='rad/s')
+        phase.add_state('energy', fix_initial=True, rate_source='e_dot', shape=(1,), ref=1, defect_ref=1, units='N**2*s')  # integration of force**2. This does not have the energy unit, but I call it "energy" anyway.
+
+        # declare control inputs
+        phase.add_control('f', fix_initial=False, rate_continuity=False, lower=-20, upper=20, shape=(1,), ref=10, units='N')
+
+        # add cart-pole parameters (set static_target=True because these params are not time-depencent)
+        phase.add_parameter('m_cart', val=1., units='kg', static_target=True)
+        phase.add_parameter('m_pole', val=0.3, units='kg', static_target=True)
+        phase.add_parameter('l_pole', val=0.5, units='m', static_target=True)
+
+        # --- set terminal constraint ---
+        # alternatively, you can impose those by setting `fix_final=True` in phase.add_state()
+        phase.add_boundary_constraint('x', loc='final', equals=1, ref=1., units='m')  # final horizontal displacement
+        phase.add_boundary_constraint('theta', loc='final', equals=np.pi, ref=1., units='rad')  # final pole angle
+        phase.add_boundary_constraint('x_dot', loc='final', equals=0, ref=1., units='m/s')  # 0 velocity at the and
+        phase.add_boundary_constraint('theta_dot', loc='final', equals=0, ref=0.1, units='rad/s')  # 0 angular velocity at the end
+        phase.add_boundary_constraint('f', loc='final', equals=0, ref=1., units='N')  # 0 force at the end
+
+        # --- set objective function ---
+        # we minimize the integral of force**2.
+        phase.add_objective('energy', loc='final', ref=100)
+
+        # --- configure optimizer ---
+        p.driver = om.pyOptSparseDriver()
+        p.driver.options['optimizer'] = 'SNOPT'
+        p.driver.options['print_results'] = False
+        # SNOPT options
+        p.driver.opt_settings['Function precision'] = 1e-14
+        p.driver.opt_settings['Major optimality tolerance'] = 1e-7
+        p.driver.opt_settings['Major feasibility tolerance'] = 1e-7
+        p.driver.opt_settings['Major iterations limit'] = 100
+        p.driver.opt_settings['Minor iterations limit'] = 100_000_000
+        p.driver.opt_settings['Iterations limit'] = 10000
+        p.driver.opt_settings['Hessian full memory'] = 1
+        p.driver.opt_settings['Hessian frequency'] = 100
+        p.driver.opt_settings['Verify level'] = -1
+
+        # declare total derivative coloring to accelerate the UDE linear solves
+        p.driver.declare_coloring()
+
+        p.setup(check=False)
+
+        # --- set initial guess ---
+        # The initial condition of cart-pole (i.e., state values at time 0) is set here because we set `fix_initial=True` when declaring the states.
+        p.set_val('traj.phase.t_initial', 0.0)  # set initial time to 0.
+        # linearly interpolate the states between initial and terminal conditions.
+        p.set_val('traj.phase.states:x', phase.interp(ys=[0., 1.], nodes='state_input'), units='m')
+        p.set_val('traj.phase.states:x_dot', phase.interp(ys=[0., 0.], nodes='state_input'), units='m/s')
+        p.set_val('traj.phase.states:theta', phase.interp(ys=[0., np.pi], nodes='state_input'), units='rad')
+        p.set_val('traj.phase.states:theta_dot', phase.interp(ys=[0., 0.], nodes='state_input'), units='rad/s')
+        p.set_val('traj.phase.states:energy', phase.interp(ys=[0., 100.], nodes='state_input'))  # start "energy" from 0.
+        p.set_val('traj.phase.controls:f', phase.interp(ys=[1., 0.], nodes='control_input'), units='N')
+            
+        # --- run optimization ---
+        dm.run_problem(p, run_driver=True, simulate=True, simulate_kwargs={'method' : 'Radau', 'times_per_seg' : 10})
+        # NOTE: with Simulate=True, dymos will call scipy.integrate.solve_ivp and simulate the trajectory using the optimized control inputs.
+
+        # --- check outputs ---
+        # objective value
+        obj = p.get_val('traj.phase.states:energy', units='N**2*s')[-1]
+        assert_near_equal(obj, 58.83924863, tolerance=1e-3)
+
+
+if __name__ == '___main__':
+    unittest.main()
