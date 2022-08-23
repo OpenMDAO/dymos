dbc2c5df3491ab9ac9c3f7ff17a09e491267a7c0
Rob Falck <rfalck@nasa.gov>
2022-08-23 11:49:25 -0400

    added matplotlib dependency to [docs]

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 # Setup optional dependencies
 optional_dependencies = {
     'docs': [
+        'matplotlib',
         'jupyter',
         'jupyter-book',
         'nbconvert',
