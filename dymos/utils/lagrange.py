8f78896a29fe023088863755a06af3a16975c332
Rob Falck <rfalck@nasa.gov>
2022-09-29 12:41:53 -0400

    major speedup of lagrange_matrices thanks to @Kenneth-T-Moore

--- dymos/utils/lagrange.py
+++ dymos/utils/lagrange.py
@@ -32,34 +32,30 @@ def lagrange_matrices(x_disc, x_interp):
     """
     nd = len(x_disc)
     ni = len(x_interp)
-    wb = np.ones(nd)
 
     Li = np.zeros((ni, nd))
     Di = np.zeros((ni, nd))
+    temp = np.zeros((ni, nd))
 
     # Barycentric Weights
+    diff = np.reshape(x_disc, (nd, 1)) - np.reshape(x_disc, (1, nd))
+    np.fill_diagonal(diff, 1.0)
+    wb = np.prod(1.0 / diff, axis=1)
+
+    # Compute Li
+    diff = np.reshape(x_interp, (ni, 1)) - np.reshape(x_disc, (1, nd))
+    for j in range(nd):
+        temp[:] = diff[:]
+        temp[:, j] = 1.0
+        Li[:, j] = wb[j] * np.prod(temp, axis=1)
+
+    # Compute Di
     for j in range(nd):
         for k in range(nd):
             if k != j:
-                wb[j] /= (x_disc[j] - x_disc[k])
-
-    # Compute Li
-    for i in range(ni):
-        for j in range(nd):
-            Li[i, j] = wb[j]
-            for k in range(nd):
-                if k != j:
-                    Li[i, j] *= (x_interp[i] - x_disc[k])
-
-    # Compute Di
-    for i in range(ni):
-        for j in range(nd):
-            for k in range(nd):
-                prod = 1.0
-                if k != j:
-                    for m in range(nd):
-                        if m != j and m != k:
-                            prod *= (x_interp[i] - x_disc[m])
-                    Di[i, j] += (wb[j] * prod)
+                temp[:] = diff[:]
+                temp[:, j] = 1.0
+                temp[:, k] = 1.0
+                Di[:, j] += wb[j] * np.prod(temp, axis=1)
 
     return Li, Di
