1bcd8d4b7bded6b0fdac44e6ae19ef6dd6422c1d
Rob Falck <rfalck@nasa.gov>
2023-04-18 09:57:49 -0400

    release 1.8.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ optional_dependencies['all'] = sorted([
 
 
 setup(name='dymos',
-    version='1.8.0-dev',
+    version='1.8.0',
     description='Open-Source Optimization of Dynamic Multidisciplinary Systems',
     long_description='''
 Dymos is a framework for the simulation and optimization of dynamical systems within the OpenMDAO Multidisciplinary Analysis and Optimization environment.
