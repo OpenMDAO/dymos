e6411ff4214d678fbd10b59187ee60308d725346
Rob Falck <rfalck@nasa.gov>
2023-08-14 20:01:35 -0400

    pep8 fixes

--- dymos/examples/brachistochrone/test/test_phase_param_introspection_failure.py
+++ dymos/examples/brachistochrone/test/test_phase_param_introspection_failure.py
@@ -14,6 +14,7 @@ class _TestEOM(om.Group):
         self.options.declare('num_nodes', types=(int,))
         self.options.declare('foo_units', allow_none=True, default=None)
         self.options.declare('foo_shapes', allow_none=True, default=None)
+        self.options.declare('foo_static', types=dict, allow_none=True, default=None)
 
     def setup(self):
         num_nodes = self.options['num_nodes']
@@ -23,12 +24,15 @@ class _TestEOM(om.Group):
 
         foo_unit = 'kg' if self.options['foo_units'] is None else self.options['foo_units']['vdot_comp']
 
+        foo_tags = ['dymos.static_target'] if self.options['foo_static']['vdot_comp'] else []
+        foo_shape = (1,) if 'dymos.static_target' in foo_tags else foo_shape
+
         vdot_comp = om.ExecComp(['vdot = g * cos(theta)',
                                  'bar = foo'],
                                 vdot={'shape': (num_nodes,), 'units': 'm/s**2'},
                                 g={'val': 9.80665, 'units': 'm/s**2'},
                                 theta={'shape': (num_nodes,), 'units': 'rad'},
-                                foo={'shape': foo_shape, 'units': foo_unit},
+                                foo={'shape': foo_shape, 'units': foo_unit, 'tags': foo_tags},
                                 bar={'shape': foo_shape, 'units': foo_unit})
 
         foo_shape = (num_nodes,) if self.options['foo_shapes'] is None \
@@ -36,12 +40,15 @@ class _TestEOM(om.Group):
 
         foo_unit = 'kg' if self.options['foo_units'] is None else self.options['foo_units']['xdot_comp']
 
+        foo_tags = ['dymos.static_target'] if self.options['foo_static']['xdot_comp'] else []
+        foo_shape = (1,) if 'dymos.static_target' in foo_tags else foo_shape
+
         xdot_comp = om.ExecComp(['xdot = v * sin(theta)',
                                  'bar = foo'],
                                 xdot={'shape': (num_nodes,), 'units': 'm/s'},
                                 v={'shape': (num_nodes,), 'units': 'm/s'},
                                 theta={'shape': (num_nodes,), 'units': 'rad'},
-                                foo={'shape': foo_shape, 'units': foo_unit},
+                                foo={'shape': foo_shape, 'units': foo_unit, 'tags': foo_tags},
                                 bar={'shape': foo_shape, 'units': foo_unit})
 
         foo_shape = (num_nodes,) if self.options['foo_shapes'] is None \
@@ -49,12 +56,15 @@ class _TestEOM(om.Group):
 
         foo_unit = 'kg' if self.options['foo_units'] is None else self.options['foo_units']['ydot_comp']
 
+        foo_tags = ['dymos.static_target'] if self.options['foo_static']['ydot_comp'] else []
+        foo_shape = (1,) if 'dymos.static_target' in foo_tags else foo_shape
+
         ydot_comp = om.ExecComp(['ydot = -v * cos(theta)',
                                  'bar = foo'],
                                 ydot={'shape': (num_nodes,), 'units': 'm/s'},
                                 v={'shape': (num_nodes,), 'units': 'm/s'},
                                 theta={'shape': (num_nodes,), 'units': 'rad'},
-                                foo={'shape': foo_shape, 'units': foo_unit},
+                                foo={'shape': foo_shape, 'units': foo_unit, 'tags': foo_tags},
                                 bar={'shape': foo_shape, 'units': foo_unit})
 
         self.add_subsystem('vdot_comp', vdot_comp)
@@ -474,6 +484,199 @@ class TestParameterUnits(unittest.TestCase):
                     "units, or explicitly provide units to the variable.")
 
         self.assertEqual(expected, str(e.exception))
+
+
+@use_tempdirs
+class TestMixedStaticDynamicParameterTargets(unittest.TestCase):
+
+    def test_all_static(self):
+        import numpy as np
+        import openmdao.api as om
+        import dymos as dm
+
+        #
+        # Define the OpenMDAO problem
+        #
+        p = om.Problem(model=om.Group())
+
+        #
+        # Define a Trajectory object
+        #
+        traj = dm.Trajectory()
+
+        p.model.add_subsystem('traj', subsys=traj)
+
+        #
+        # Define a Dymos Phase object with GaussLobatto Transcription
+        #
+        phase = dm.Phase(ode_class=_TestEOM,
+                         transcription=dm.GaussLobatto(num_segments=10, order=3),
+                         ode_init_kwargs={'foo_static': {'xdot_comp': True, 'ydot_comp': True, 'vdot_comp': True}})
+
+        traj.add_phase(name='phase0', phase=phase)
+
+        #
+        # Set the time options
+        # Time has no targets in our ODE.
+        # We fix the initial time so that it is not a design variable in the optimization.
+        # The duration of the phase is allowed to be optimized, but is bounded on [0.5, 10].
+        #
+        phase.set_time_options(fix_initial=True, duration_bounds=(0.5, 10.0), units='s')
+
+        #
+        # Set the time options
+        # Initial values of positions and velocity are all fixed.
+        # The final value of position are fixed, but the final velocity is a free variable.
+        # The equations of motion are not functions of position, so 'x' and 'y' have no targets.
+        # The rate source points to the output in the ODE which provides the time derivative of the
+        # given state.
+        phase.add_state('x', fix_initial=True, fix_final=True, units='m',
+                        rate_source='xdot_comp.xdot')
+        phase.add_state('y', fix_initial=True, fix_final=True, units='m',
+                        rate_source='ydot_comp.ydot')
+        phase.add_state('v', fix_initial=True, fix_final=False, units='m/s',
+                        rate_source='vdot_comp.vdot', targets=['xdot_comp.v', 'ydot_comp.v'])
+
+        # Define theta as a control.
+        phase.add_control(name='theta', units='rad', lower=0, upper=np.pi,
+                          targets=['xdot_comp.theta', 'ydot_comp.theta', 'vdot_comp.theta'])
+
+        phase.add_parameter('foo',
+                            units='lbm',
+                            opt=False,
+                            targets=['xdot_comp.foo', 'ydot_comp.foo', 'vdot_comp.foo'])
+
+        # Minimize final time.
+        phase.add_objective('time', loc='final')
+
+        # Set the driver.
+        p.driver = om.ScipyOptimizeDriver()
+
+        # Allow OpenMDAO to automatically determine our sparsity pattern.
+        # Doing so can significant speed up the execution of Dymos.
+        p.driver.declare_coloring()
+
+        # Setup the problem
+        p.setup(check=True)
+
+        # Now that the OpenMDAO problem is setup, we can set the values of the states.
+        p.set_val('traj.phase0.t_initial', 0.0, units='s')
+        p.set_val('traj.phase0.t_duration', 2.0, units='s')
+        p.set_val('traj.phase0.states:x', phase.interp('x', [0, 10]), units='m')
+        p.set_val('traj.phase0.states:y', phase.interp('y', [10, 5]), units='m')
+        p.set_val('traj.phase0.states:v', phase.interp('v', [0, 5]), units='m/s')
+        p.set_val('traj.phase0.controls:theta', phase.interp('theta', [90, 90]), units='deg')
+        p.set_val('traj.phase0.parameters:foo', 5.0)
+
+        # Run the driver to solve the problem
+        p.run_driver()
+
+        expected = convert_units(5.0, 'lbm', 'kg')
+
+        self.assertEqual((1,), phase.parameter_options['foo']['shape'])
+        assert_near_equal(p.get_val('traj.phase0.timeseries.time')[-1], 1.8016, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.parameter_vals:foo')[-1], 5.0, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_col.xdot_comp.foo'), expected, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_col.ydot_comp.foo'), expected, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_col.vdot_comp.foo'), expected, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_disc.xdot_comp.foo'), expected, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_disc.ydot_comp.foo'), expected, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_disc.vdot_comp.foo'), expected, tolerance=1.0E-5)
+
+    def test_mixed_static(self):
+        import numpy as np
+        import openmdao.api as om
+        import dymos as dm
+
+        #
+        # Define the OpenMDAO problem
+        #
+        p = om.Problem(model=om.Group())
+
+        #
+        # Define a Trajectory object
+        #
+        traj = dm.Trajectory()
+
+        p.model.add_subsystem('traj', subsys=traj)
+
+        #
+        # Define a Dymos Phase object with GaussLobatto Transcription
+        #
+        phase = dm.Phase(ode_class=_TestEOM,
+                         transcription=dm.GaussLobatto(num_segments=10, order=3),
+                         ode_init_kwargs={'foo_static': {'xdot_comp': True, 'ydot_comp': True, 'vdot_comp': False}})
+
+        traj.add_phase(name='phase0', phase=phase)
+
+        #
+        # Set the time options
+        # Time has no targets in our ODE.
+        # We fix the initial time so that it is not a design variable in the optimization.
+        # The duration of the phase is allowed to be optimized, but is bounded on [0.5, 10].
+        #
+        phase.set_time_options(fix_initial=True, duration_bounds=(0.5, 10.0), units='s')
+
+        #
+        # Set the time options
+        # Initial values of positions and velocity are all fixed.
+        # The final value of position are fixed, but the final velocity is a free variable.
+        # The equations of motion are not functions of position, so 'x' and 'y' have no targets.
+        # The rate source points to the output in the ODE which provides the time derivative of the
+        # given state.
+        phase.add_state('x', fix_initial=True, fix_final=True, units='m',
+                        rate_source='xdot_comp.xdot')
+        phase.add_state('y', fix_initial=True, fix_final=True, units='m',
+                        rate_source='ydot_comp.ydot')
+        phase.add_state('v', fix_initial=True, fix_final=False, units='m/s',
+                        rate_source='vdot_comp.vdot', targets=['xdot_comp.v', 'ydot_comp.v'])
+
+        # Define theta as a control.
+        phase.add_control(name='theta', units='rad', lower=0, upper=np.pi,
+                          targets=['xdot_comp.theta', 'ydot_comp.theta', 'vdot_comp.theta'])
+
+        phase.add_parameter('foo',
+                            units='lbm',
+                            opt=False,
+                            targets=['xdot_comp.foo', 'ydot_comp.foo', 'vdot_comp.foo'])
+
+        # Minimize final time.
+        phase.add_objective('time', loc='final')
+
+        # Set the driver.
+        p.driver = om.ScipyOptimizeDriver()
+
+        # Allow OpenMDAO to automatically determine our sparsity pattern.
+        # Doing so can significant speed up the execution of Dymos.
+        p.driver.declare_coloring()
+
+        # Setup the problem
+        p.setup(check=True)
+
+        # Now that the OpenMDAO problem is setup, we can set the values of the states.
+        p.set_val('traj.phase0.t_initial', 0.0, units='s')
+        p.set_val('traj.phase0.t_duration', 2.0, units='s')
+        p.set_val('traj.phase0.states:x', phase.interp('x', [0, 10]), units='m')
+        p.set_val('traj.phase0.states:y', phase.interp('y', [10, 5]), units='m')
+        p.set_val('traj.phase0.states:v', phase.interp('v', [0, 5]), units='m/s')
+        p.set_val('traj.phase0.controls:theta', phase.interp('theta', [90, 90]), units='deg')
+        p.set_val('traj.phase0.parameters:foo', 5.0)
+
+        # Run the driver to solve the problem
+        p.run_driver()
+
+        expected = convert_units(5.0, 'lbm', 'kg')
+
+        self.assertEqual((1,), phase.parameter_options['foo']['shape'])
+        self.assertEqual({'xdot_comp.foo', 'ydot_comp.foo'}, phase.parameter_options['foo']['static_targets'])
+        assert_near_equal(p.get_val('traj.phase0.timeseries.time')[-1], 1.8016, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.parameter_vals:foo')[-1], 5.0, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_col.xdot_comp.foo'), expected, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_col.ydot_comp.foo'), expected, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_col.vdot_comp.foo'), expected*np.ones(10), tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_disc.xdot_comp.foo'), expected, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_disc.ydot_comp.foo'), expected, tolerance=1.0E-5)
+        assert_near_equal(p.get_val('traj.phase0.rhs_disc.vdot_comp.foo'), expected*np.ones(20), tolerance=1.0E-5)
 
 
 if __name__ == '__main__':  # pragma: no cover
