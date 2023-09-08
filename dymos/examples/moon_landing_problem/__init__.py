c106ce485d7b794f0d86849e7b0bd0844f3b58c7
Kaushik Ponnapalli <kaushik.s.ponnapalli@nasa.gov>
2023-09-08 13:44:26 -0500

    Added moon landing problem from Ross's paper

---
+++ dymos/examples/moon_landing_problem/__init__.py
@@ -0,0 +1,1 @@
+from .moon_landing_problem_ode import MoonLandingProblemODE
---
+++ dymos/examples/moon_landing_problem/test/test_moon_landing_problem.py
@@ -0,0 +1,79 @@
+import openmdao.api as om
+import dymos as dm
+import unittest
+
+from openmdao.utils.assert_utils import assert_near_equal
+from openmdao.utils.testing_utils import use_tempdirs, require_pyoptsparse
+from dymos.examples.moon_landing_problem import MoonLandingProblemODE
+
+
+# @use_tempdirs
+class TestMoonLandingProblem(unittest.TestCase):
+
+    @require_pyoptsparse(optimizer='IPOPT')
+    def make_problem(self, grid_type):
+        self.p = om.Problem(model=om.Group())
+        self.p.driver = om.pyOptSparseDriver()
+        self.p.driver.declare_coloring()
+        self.p.driver.options['optimizer'] = 'IPOPT'
+        self.p.driver.opt_settings['hessian_approximation'] = 'limited-memory'
+        self.p.driver.opt_settings['print_level'] = 0
+        self.p.driver.opt_settings['linear_solver'] = 'mumps'
+        self.p.driver.declare_coloring()
+
+        t = dm.Birkhoff(grid=dm.BirkhoffGrid(num_segments=1, nodes_per_seg=80, grid_type=grid_type))
+
+        traj = self.p.model.add_subsystem('traj', dm.Trajectory())
+        phase = dm.Phase(ode_class=MoonLandingProblemODE,
+                         transcription=t)
+
+        phase.set_time_options(fix_initial=True, fix_duration=False)
+        phase.add_state('h', fix_initial=True, rate_source='h_dot')
+        phase.add_state('v', fix_initial=True, rate_source='v_dot')
+        phase.add_state('m', fix_initial=True, lower=1e-3, rate_source='m_dot')
+        phase.add_control('T', lower=0.0, upper=1.227)
+
+        phase.add_boundary_constraint('h', loc='final', equals=0.0)
+        phase.add_boundary_constraint('v', loc='final', equals=0.0)
+
+        phase.add_objective('J=1-m')
+        phase.set_simulate_options(atol=1.0E-2, rtol=1.0E-2)
+
+        traj.add_phase('phase', phase)
+
+        self.p.setup(check=True, force_alloc_complex=True)
+
+        self.p.set_val('traj.phase.initial_states:h', 1.0)
+        self.p.set_val('traj.phase.initial_states:v', -0.783)
+        self.p.set_val('traj.phase.initial_states:m', 1.0)
+        self.p.set_val('traj.phase.t_initial', 0)
+        self.p.set_val('traj.phase.t_duration', 1.0)
+        self.p.set_val('traj.phase.controls:T', phase.interp('T', [0.0, 1.227]))
+
+    def test_problem_lgl(self):
+        self.make_problem(grid_type='lgl')
+        dm.run_problem(self.p, simulate=True)
+        h = self.p.get_val('traj.phase.timeseries.h')
+        v = self.p.get_val('traj.phase.timeseries.v')
+        T = self.p.get_val('traj.phase.timeseries.T')
+
+        assert_near_equal(T[0], 0.0, tolerance=1e-5)
+        assert_near_equal(T[20], 0.0, tolerance=1e-5)
+        assert_near_equal(T[30], 1.227, tolerance=1e-5)
+        assert_near_equal(T[-1], 1.227, tolerance=1e-5)
+        assert_near_equal(h[-1], 0.0, tolerance=1e-5)
+        assert_near_equal(v[-1], 0.0, tolerance=1e-5)
+
+    def test_problem_cgl(self):
+        self.make_problem(grid_type='cgl')
+        dm.run_problem(self.p)
+        h = self.p.get_val('traj.phase.timeseries.h')
+        v = self.p.get_val('traj.phase.timeseries.v')
+        T = self.p.get_val('traj.phase.timeseries.T')
+
+        assert_near_equal(T[0], 0.0, tolerance=1e-5)
+        assert_near_equal(T[20], 0.0, tolerance=1e-5)
+        assert_near_equal(T[30], 1.227, tolerance=1e-5)
+        assert_near_equal(T[-1], 1.227, tolerance=1e-5)
+        assert_near_equal(h[-1], 0.0, tolerance=1e-5)
+        assert_near_equal(v[-1], 0.0, tolerance=1e-5)
