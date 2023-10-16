9e02030dc76183c97cfbf2bcc671e109f4c3d139
Rob Falck <rfalck@nasa.gov>
2023-10-16 09:09:03 -0400

    Fixed python 3.12 compatibility issues. (#1004)
    * correctly handle jax version == "latest"
    
    * Changed playwright to only be a dependency if python version is less than 3.12 due to an incompatibility in its dependencies
    
    * New pycodestyle issues with 3.12.
    unittest.TestCase.assertEquals -> assertEqual

--- dymos/visualization/linkage/test/test_gui.py
+++ dymos/visualization/linkage/test/test_gui.py
@@ -1,9 +1,16 @@
 """Test Dymos Linkage report GUI with using Playwright."""
 import unittest
 import os
+try:
+    import playwright
+except ImportError:
+    playwright = None
 
-os.system("playwright install")
-from linkage_report_ui_test import dymos_linkage_gui_test_case  # nopep8: E402
+
+if playwright is not None:
+    os.system("playwright install")
+    from linkage_report_ui_test import dymos_linkage_gui_test_case  # nopep8: E402
+
 
 if __name__ == "__main__":
     unittest.main()
