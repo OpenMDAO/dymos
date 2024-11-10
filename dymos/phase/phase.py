fc8c84954d2cf0ea43fc00b29c5e2e7f49cb1ab1
Rob Falck <rfalck@nasa.gov>
2024-11-10 09:16:51 -0500

    Debugging mpi tests on github latest matrix

--- dymos/phase/phase.py
+++ dymos/phase/phase.py
@@ -1912,6 +1912,7 @@ class Phase(om.Group):
         units : str or None
             Units of the time. If none are specified, the default units are used.
         """
+        print(f'Setting times {self.pathname=} {initial=} {duration=}')
         if units is None:
             units = self.time_options['units']
         if initial is not None:
