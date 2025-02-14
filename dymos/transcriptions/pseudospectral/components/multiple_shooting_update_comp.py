some fixes for derivatives.

dymos/transcriptions/pseudospectral/components/multiple_shooting_update_comp.py
@@ -97,8 +97,8 @@ class MultipleShootingUpdateComp(om.ExplicitComponent):
         num_nodes_last_seg = gd.subset_num_nodes_per_segment['all'][-1]
 
         for iseg in range(gd.num_segments - 1):
-            rows_i = np.arange(seg_end_nodes[iseg], seg_end_nodes[iseg + 1], dtype=int)
-            cols_i = seg_end_nodes[iseg] * np.ones(nodes_per_seg[iseg])
+            rows_i = np.arange(seg_start_nodes[iseg], seg_start_nodes[iseg] + nodes_per_seg[iseg], dtype=int)
+            cols_i = seg_start_nodes[iseg+1] * np.ones(nodes_per_seg[iseg])
             data_i = np.ones(nodes_per_seg[iseg])
 
             rows = np.concatenate((rows, rows_i))
@@ -121,8 +121,6 @@ class MultipleShootingUpdateComp(om.ExplicitComponent):
             shape = options['shape']
             units = options['units']
             size = np.prod(shape)
-
-            ar_size = np.arange(size, dtype=int)
 
             rate_units = get_rate_units(units, time_units)
             var_names = self._var_names[state_name]
@@ -149,16 +147,19 @@ class MultipleShootingUpdateComp(om.ExplicitComponent):
                     units=units
                 )
 
+                ar_size_x_nn0 = np.arange(num_nodes_first_seg * size, dtype=int)
                 self.declare_partials(of=var_names['x_0'],
                                       wrt=var_names['x_a'],
-                                      rows=ar_size, cols=ar_size, val=1.0)
+                                      rows=ar_size_x_nn0,
+                                      cols=np.zeros_like(ar_size_x_nn0),
+                                      val=1.0)
 
-                rs, cs = self._M_fwd.nonzero()
-                self.declare_partials(of=var_names['x_0'],
-                                      wrt=var_names['x'],
-                                      rows=rs[num_nodes_first_seg:],
-                                      cols=cs[num_nodes_first_seg:],
-                                      val=self._M_fwd.data[num_nodes_first_seg:])
+                if num_segs > 1:
+                    rs, cs = self._M_fwd.nonzero()
+                    self.declare_partials(of=var_names['x_0'],
+                                        wrt=var_names['x'],
+                                        rows=rs, cols=cs,
+                                        val=self._M_fwd.data)
 
             elif options['solve_segments'] == 'backward':
 
@@ -177,17 +178,20 @@ class MultipleShootingUpdateComp(om.ExplicitComponent):
                     units=units
                 )
 
+                ar_size_x_nnf = np.arange(num_nodes_last_seg * size, dtype=int)
+                rs = size * (num_nodes - num_nodes_last_seg) + ar_size_x_nnf
                 self.declare_partials(of=var_names['x_f'],
                                       wrt=var_names['x_b'],
-                                      rows=ar_size + (num_segs - 1) * size,
-                                      cols=ar_size)
+                                      rows=rs,
+                                      cols=np.zeros(num_nodes_last_seg * size),
+                                      val=1.0)
 
-                rs, cs = self._M_bkwd.nonzero()
-                self.declare_partials(of=var_names['x_f'],
-                                      wrt=var_names['x'],
-                                      rows=rs[:-num_nodes_last_seg],
-                                      cols=cs[:-num_nodes_last_seg],
-                                      val=self._M_fwd.data[:-num_nodes_last_seg])
+                if num_segs > 1:
+                    rs, cs = self._M_bkwd.nonzero()
+                    self.declare_partials(of=var_names['x_f'],
+                                          wrt=var_names['x'],
+                                          rows=rs, cols=cs,
+                                          val=self._M_bkwd)
 
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):

dymos/transcriptions/pseudospectral/components/test/test_multiple_shooting_iter_group.py
@@ -127,7 +127,7 @@ class TestMultipleShootingIterGroup(unittest.TestCase):
     def test_multiple_shooting_iter_group(self):
         for direction in ['forward']:
             for grid_type in [GaussLobattoGrid, ChebyshevGaussLobattoGrid]:
-                for nl_solver in ['nlbgs']:
+                for nl_solver in [om.NonlinearBlockGS(use_aitken=True), om.NewtonSolver(solve_subsystems=True)]:
                     with self.subTest(msg=grid_type):
                         with dymos.options.temporary(include_check_partials=True):
 
@@ -164,7 +164,12 @@ class TestMultipleShootingIterGroup(unittest.TestCase):
 
                             ms = p.model._get_subsystem('ms')
 
+                            ms.nonlinear_solver = nl_solver
+                            ms.linear_solver = om.DirectSolver()
+
                             p.setup(force_alloc_complex=True)
+
+
 
                             # Instead of using the TimeComp just transform the node segment taus onto [0, 2]
                             times = (grid_data.node_stau + 1) * 1.0
@@ -201,8 +206,8 @@ class TestMultipleShootingIterGroup(unittest.TestCase):
                             # assert_near_equal(solution[np.newaxis, 0], p.get_val('ms.initial_states:x'), tolerance=1.0E-9)
                             # assert_near_equal(solution[np.newaxis, -1], p.get_val('ms.final_states:x'), tolerance=1.0E-9)
 
-                            # cpd = p.check_partials(method='fd', compact_print=False, out_stream=None)
-                            # assert_check_partials(cpd, atol=1.0E-5, rtol=1.0E-5)
+                            cpd = p.check_partials(method='fd', compact_print=False, out_stream=None)
+                            assert_check_partials(cpd, atol=1.0E-5, rtol=1.0E-5)
 
     # @unittest.skip('This test is a demonstation of the inability of Birkhoff-Picard '
     #                'iteration to solve highly nonlinear systems.')